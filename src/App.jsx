9df66a0ce9540b6ab55843e55683ad32dd942b8b
CocoaHoto <kapezovdaulet@gmail.com>
2023-12-03 20:16:59 -0500

    Implemented the internal logic of the DeleteCart, IncrementQuantity and DecrementQuantity  functions. Created the necessary blocks that are presented on the Cart Page in Figma such as ShipmentAddress, PaymentMethod, Cart, CartSummary and Returns. I have added React Router in order to navigate to other pages from Cart page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import './index.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Header from './components/Header/Header';
+import Cart from './pages/Buyers/CartPage';
 
 function App() {
+  const carts = [
+    { id: 1, product: { avatar: '...', name: 'Product 1', description: '...', price: 10 }, number_of_product: 2 },
+    { id: 2, product: { avatar: '...', name: 'Product 2', description: '...', price: 20 }, number_of_product: 1 },
+    // ...more cart items
+  ];
 
   return (
     <>
@@ -13,6 +19,7 @@ function App() {
           <Header />
           <Routes>
             <Route exact path='/' element={<LandingPage />}/>
+            <Route path='/cart' element={<Cart initialCarts={carts} />}/>
             <Route path='/sellers' element="#"/>
             <Route path='/help' element="#"/>
             <Route path='/language' element="#"/>

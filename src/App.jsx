6ace29488c770754aa543cf8fce08c240106cf6f
yabetsg <yabets.g2@gmail.com>
2023-12-04 21:27:12 -0500

    Create signup page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Cart from './pages/Buyers/CartPage';
 import LoginPage from './pages/Buyers/LoginPage';
+import SignupPage from './pages/Buyers/SignupPage';
 
 function App() {
   return (
@@ -21,7 +22,7 @@ function App() {
             <Route path='/account' element="#"/>
             <Route path='/app' element="#"/>
             <Route path='/login' element={<LoginPage/>}/>
-            <Route path='/signup' element="#"/>
+            <Route path='/signup' element={<SignupPage/>}/>
           </Routes>
         </Router>
         
--- src/pages/Buyers/SignupPage.jsx
+++ src/pages/Buyers/SignupPage.jsx
@@ -2,18 +2,30 @@ import facebook from "../../assets/ICONS/socials/facebook.svg";
 import twitter from "../../assets/ICONS/socials/twitter.svg";
 import linkedin from "../../assets/ICONS/socials/linkedin.svg";
 import instagram from "../../assets/ICONS/socials/instagram.svg";
-
-const LoginPage = () => {
+const SignupPage = () => {
   const socialMedia = [facebook, instagram, linkedin, twitter];
-
   return (
-    <div className="flex flex-col items-center mb-8 font-['Roboto'] ">
+    <div className="flex flex-col items-center mb-8 font-['Roboto']">
       <div className="flex flex-col gap-4 p-4 text-center">
+        <span className="font-semibold">Welcome to SONNY</span>
       </div>
-      <div className="flex flex-col max-w-[704px] w-full border-[0.5px] border-[#938F96] p-10">
+      <div className="flex flex-col max-w-[704px] w-full border-[0.5px] border-[#938F96] p-10 ">
         <form action="" className="flex flex-col gap-5">
           <div className="flex flex-col gap-2">
-            <label htmlFor="email" className="font-semibold ">
+            <label htmlFor="name" className="font-semibold">
+              Your name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="First and last name"
+              className="p-3 px-5 border-[0.5px] border-[#938F96] rounded-md"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="font-semibold">
               Email
             </label>
             <input
@@ -38,6 +50,19 @@ const LoginPage = () => {
             />
           </div>
 
+          <div className="flex flex-col gap-2">
+            <label htmlFor="confirm_password" className="font-semibold">
+              Re-enter password
+            </label>
+            <input
+              type="password"
+              name="confirm_password"
+              id="confirm_password"
+              placeholder="Confirm password"
+              className="p-3 px-5 border-[0.5px] border-[#938F96] rounded-md"
+            />
+          </div>
+
           <div className="flex gap-2">
             <input type="checkbox" name="" id="" />
             <span>Stay signed in</span>
@@ -45,27 +70,27 @@ const LoginPage = () => {
 
           <button
             type="submit"
-            className="bg-[#EEC643] px-8 py-4 max-w-[704px] w-full mx-auto mt-8 rounded-md"
+            className="bg-[#EEC643] px-8 py-4 max-w-[472px] w-full mx-auto mt-8 rounded-md"
           >
-            Log in
+            Create
           </button>
         </form>
 
         <div className="py-8 text-center">
-          Dont have an account?{" "}
-          <a href="/signup" className="p-2 underline">
-            Sign up
+          Already have an account?
+          <a href="/login" className="p-2 underline text">
+            Log in
           </a>
         </div>
 
         <div>
           <div className="flex items-center">
             <div className="flex-1 h-px bg-[#938F96]"></div>
-            <div className="mx-4 text-sm font-['Roboto']">or continue with</div>
+            <div className="mx-4 text-sm">or create with</div>
             <div className="flex-1 h-px bg-[#938F96]"></div>
           </div>
           <div className="flex justify-center py-10 gap-9">
-            {socialMedia.map((media,i)=>{
+           {socialMedia.map((media,i)=>{
               return <img key={i} src={media}/>
             })}
           </div>
@@ -75,4 +100,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default SignupPage;

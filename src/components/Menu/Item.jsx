0848c0082522b75edba6e8c4d95ef1d04a5a5870
Krygore <webdev.krygore@gmail.com>
2023-12-06 15:25:41 -0800

    updated Item.jsx

---
+++ src/components/Menu/Item.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+const Item = ({ children }) => {
+  return (
+    <div className="flex gap-5 w-[316px] px-2 py-3 items-center">
+      <input type="checkbox" name="" id="" className="w-6 h-6" />
+      <span className="font-[Roboto] font-normal text-base">{children}</span>
+    </div>
+  );
+};
+
+Item.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Item;

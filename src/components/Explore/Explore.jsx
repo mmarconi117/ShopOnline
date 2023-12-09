98c05ece65f6864da0e98fc99a82f96e04804e89
Kenneth Fisher <kenandcrys@live.com>
2023-12-08 19:40:03 -0600

    WIP LandingPage

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,11 +1,84 @@
 
 import { ExploreItem } from "./ExploreItem";
 import beauty from "./assets/beauty.png";
+import bagsAndShoes from './assets/bagsAndShoes.png';
+import accessories from './assets/accessories.png';
+import electronics from './assets/electronics.png';
+import fashion from './assets/fashion.png'
+import furniture from './assets/furniture.png';
+import groceries from './assets/groceries.png';
+import otherCategories from './assets/otherCategories.png';
+import skinCare from './assets/skinCare.png';
+import wristWatches from './assets/wristWatches.png'
+import mensSneakers from './assets/sneakers.png'
+import health from './assets/health.png'
+
+const baseImages = [
+  {
+    category: 'Beauty',
+    img: beauty,
+  },
+  {
+    category: 'Fashion',
+    img: fashion,
+  },
+  {
+    category: 'Accessories',
+    img: accessories,
+  },
+  {
+    category: 'Furniture',
+    img: furniture,
+  },
+  {
+    category: 'Wristwatches',
+    img: wristWatches,
+  },
+  {
+    category: `Men's sneakers`,
+    img: mensSneakers,
+  },
+  {
+    category: 'Groceries',
+    img: groceries,
+  },
+  {
+    category: 'Health',
+    img: health,
+  },
+  {
+    category: 'Electronics',
+    img: electronics,
+  },
+  {
+    category: 'Skincare',
+    img: skinCare,
+  },
+  {
+    category: 'Bags & Shoes',
+    img: bagsAndShoes,
+  },
+  {
+    category: 'Other categories',
+    img: otherCategories,
+  },
+  
+]
+
 
 export const Explore = () => {
-  const exploreItems = Array.from({ length: 12 }, (_, i) => (
-    <ExploreItem key={i} src={beauty} title={"Beauty"} />
-  ));
+  
+  const exploreItems = baseImages.map((item, i) => {
+    return (
+      <span key={i}>
+        <ExploreItem key={i} src={item.img} title={item.category} />
+      </span>
+    )
+    
+  })
+
+
+
 
   return (
     <div className="flex h-fit px-0 pb-6 flex-col items-start justify-center gap-[18px] flex-1 bg-[#BAD9E8]">

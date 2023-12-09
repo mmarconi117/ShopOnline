c70f034a474dc4379cf393f42244f408d0c1afe5
Kenneth Fisher <kenandcrys@live.com>
2023-12-08 18:26:54 -0600

    WIP Explore.jsx

--- src/components/Explore/ExploreItem.jsx
+++ src/components/Explore/ExploreItem.jsx
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
 
-export  const ExploreItem = ({src,title}) => {
+export const ExploreItem = ({ src, title }) => {
   return (
-    <div className="flex w-[219px] flex-col pt-[10px] pb-5 px-[10x] justify-center items-center bg-[#FBFDFE] gap-[17px]">
-        <img src={src} alt="" className="w-[197px] h-[179px] rounded-sm"/>
-        <div className="text-[#2284B6] text-center text-xl font-semibold font-[Roboto]">{title}</div>
+    <div className="flex flex-col pb-2 px-[10px] pr-4 justify-between items-center bg-[#FBFDFE] rounded-md shadow-md" style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)' }}>
+      <img
+        src={src}
+        alt=""
+        className="w-[197px] h-[179px] rounded-md"
+        style={{ borderRadius: '10px', marginTop: '20px' }}
+      />
+      <div className="text-[#2284B6] text-center text-xl font-semibold font-[Roboto]">{title}</div>
     </div>
-  )
-
-}
+  );
+};
 
 ExploreItem.propTypes = {
   src: PropTypes.string.isRequired,

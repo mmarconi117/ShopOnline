4bd3c1f2a0bbb4143e960fe90df8d7cafa51a17e
Wesley-waka <wesleywaka77@gmail.com>
2023-12-13 15:39:08 +0300

    company service page layout

---
+++ src/pages/Sellers/CompanyService.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CompanyService = () => {
+  return (
+    <div className="bg-gray-200 h-screen flex flex-row space-x-2">
+      {/* Dashboard */}
+      <div className="w-72 bg-white flex-col space-y-2">
+        <div></div>
+        <div className="flex flex-col space-y-4"></div>
+      </div>
+      {/* Dashboard */}
+      {/* Company Service form */}
+      <div></div>
+      {/* Company Service form */}
+    </div>
+  );
+};
+
+export default CompanyService;

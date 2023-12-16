6ab5a8d11fdfe56d5b016b30c6833ed32e91db1d
Kenneth Fisher <kenandcrys@live.com>
2023-12-16 15:39:28 -0600

    Added P tag for descrition

--- src/components/TopPicks/ProductCard.jsx
+++ src/components/TopPicks/ProductCard.jsx
@@ -6,7 +6,7 @@ const ProductCard = (props) => {
     <div className="flex w-[219px] px-[10px] pt-[10px] pb-4 flex-col items-start gap-6">
       <img src={image} alt={description} />
       <div className="font-[Roboto] text-sm font-normal leading-[20px] text-black">
-        {description}
+        <p>{description}</p>
       </div>
       <div className="font-[Arial] text-sm font-normal tracking-[0.25px]">
         from $<span className="text-xl tracking-[0.15px] leading-[24px] font-bold text-[#09618E]">{price}</span>

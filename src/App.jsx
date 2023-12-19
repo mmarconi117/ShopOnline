17e8d903e9b15ea86f37dad8ef20e2b70fa55007
jonathanj101 <jonatjimen@gmail.com>
2023-12-19 13:43:01 -0500

    prettier formatted, imported product details component

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,65 @@ import Header from './components/Header/Header';
 import Cart from './pages/Buyers/CartPage';
 import LoginPage from './pages/Buyers/LoginPage';
 import SignupPage from './pages/Buyers/SignupPage';
+import ProductDetails from "./pages/Buyers/ProductDetails";
 
 function App() {
-  return (
-    <>
-      <div>
-        <Router>
-          <Header />
-          <Routes>
-            <Route exact path='/' element={<LandingPage />}/>
-            <Route path='/subcategories' element={<SubCategory/>}/>
-            <Route path='/cart' element={<Cart/>}/>
-            <Route path='/sellers' element="#"/>
-            <Route path='/help' element="#"/>
-            <Route path='/language' element="#"/>
-            <Route path='/account' element="#"/>
-            <Route path='/app' element="#"/>
-            <Route path='/login' element={<LoginPage/>}/>
-            <Route path='/signup' element={<SignupPage/>}/>
-          </Routes>
-        </Router>
-        
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <Router>
+                    <Header />
+                    <Routes>
+                        <Route
+                            exact
+                            path="/"
+                            element={<LandingPage />}
+                        />
+                        <Route
+                            path="/subcategories"
+                            element={<SubCategory />}
+                        />
+                        <Route
+                            path="/product-details"
+                            element={<ProductDetails />}
+                        />
+                        <Route
+                            path="/cart"
+                            element={<Cart />}
+                        />
+                        <Route
+                            path="/sellers"
+                            element="#"
+                        />
+                        <Route
+                            path="/help"
+                            element="#"
+                        />
+                        <Route
+                            path="/language"
+                            element="#"
+                        />
+                        <Route
+                            path="/account"
+                            element="#"
+                        />
+                        <Route
+                            path="/app"
+                            element="#"
+                        />
+                        <Route
+                            path="/login"
+                            element={<LoginPage />}
+                        />
+                        <Route
+                            path="/signup"
+                            element={<SignupPage />}
+                        />
+                    </Routes>
+                </Router>
+            </div>
+        </>
+    );
 }
 
 export default App

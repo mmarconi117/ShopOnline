9fc06fc1a8a2f08d118e3373b0c1113efc8dff40
Aaron Yii <aaronyii@mail.usf.edu>
2023-12-21 17:19:31 -0600

    Integrated mobile design to login page

--- src/pages/Buyers/LoginPage.jsx
+++ src/pages/Buyers/LoginPage.jsx
@@ -7,13 +7,13 @@ const LoginPage = () => {
   const socialMedia = [facebook, instagram, linkedin, twitter];
 
   return (
-    <div className="flex flex-col items-center mb-8 font-['Roboto'] ">
+    <div className="flex flex-col items-center mb-8 font-['Roboto'] max-sm:px-4 max-sm:py-12">
       <div className="flex flex-col gap-4 p-4 text-center">
       </div>
-      <div className="flex flex-col max-w-[704px] w-full border-[0.5px] border-[#938F96] p-10">
-        <form action="" className="flex flex-col gap-5">
+      <div className="flex flex-col max-w-[704px] w-full border-0 sm:border-[0.5px] sm:border-[#938F96] sm:p-10">
+        <form action="" className="flex flex-col gap-5 max-sm:gap-6 leading-6">
           <div className="flex flex-col gap-2">
-            <label htmlFor="email" className="font-semibold ">
+            <label htmlFor="email" className="font-semibold max-sm:font-medium max-sm:text-[14px] leading-6 max-sm:leading-[22px]">
               Email
             </label>
             <input
@@ -21,12 +21,12 @@ const LoginPage = () => {
               name="email"
               id="email"
               placeholder="Enter email"
-              className="p-3 px-5 border-[0.5px] border-[#938F96] rounded-md"
+              className="p-3 pl-5 border-[1px] border-[#938F96] rounded-md"
             />
           </div>
 
           <div className="flex flex-col gap-2">
-            <label htmlFor="password" className="font-semibold">
+            <label htmlFor="password" className="font-semibold max-sm:font-medium max-sm:text-[14px] max-sm:leading-[22px]">
               Password
             </label>
             <input
@@ -34,26 +34,26 @@ const LoginPage = () => {
               name="password"
               id="password"
               placeholder="Enter password"
-              className="p-3 px-5 border-[0.5px] border-[#938F96] rounded-md"
+              className="p-3 pl-5 border-[1px] border-[#938F96] rounded-md"
             />
           </div>
 
-          <div className="flex gap-2">
+          <div className="flex gap-2 font-normal max-sm:leading-4 max-sm:text-xs">
             <input type="checkbox" name="" id="" />
             <span>Stay signed in</span>
           </div>
 
           <button
             type="submit"
-            className="bg-[#EEC643] px-8 py-4 max-w-[704px] w-full mx-auto mt-8 rounded-md"
+            className="bg-[#EEC643] px-8 py-4 max-sm:py-[14px] max-sm:px-[30px] max-w-[704px] sm:font-semibold w-full mx-auto mt-8 rounded-md max-sm:text-sm"
           >
             Log in
           </button>
         </form>
 
         <div className="py-8 text-center">
-          Dont have an account?{" "}
-          <a href="/signup" className="p-2 underline">
+          Dont have an account?
+          <a href="/signup" className="p-2 underline text-[#1F2070] ">
             Sign up
           </a>
         </div>
@@ -66,7 +66,7 @@ const LoginPage = () => {
           </div>
           <div className="flex justify-center py-10 gap-9">
             {socialMedia.map((media,i)=>{
-              return <img key={i} src={media}/>
+              return <img key={i} src={media} className="w-12 h-12 max-sm:w-8 max-sm:h-8"/>
             })}
           </div>
         </div>

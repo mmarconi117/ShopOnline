6d585f9806b6a14849c468143cfebc18c71e707a
jonathanj101 <jonatjimen@gmail.com>
2023-12-26 18:47:33 -0500

    separated nav items and added sub links, for readability

---
+++ src/components/NavigationBar/Links.jsx
@@ -0,0 +1,78 @@
+export let Links = [
+    {
+        name: "See Deals",
+        url: "",
+        subMenu: false,
+        subLinks: [
+            {
+                name: "test",
+                url: "",
+            },
+            {
+                name: "test",
+                url: "",
+            },
+            {
+                name: "test",
+                url: "",
+            },
+        ],
+    },
+    {
+        name: "Latest arrivals",
+        url: "",
+        subMenu: false,
+        subLinks: [
+            {
+                name: "test",
+                url: "",
+            },
+            {
+                name: "test",
+                url: "",
+            },
+            {
+                name: "test",
+                url: "",
+            },
+        ],
+    },
+    {
+        name: "See Products",
+        url: "",
+        subMenu: false,
+        subLinks: [
+            {
+                name: "test",
+                url: "",
+            },
+            {
+                name: "test",
+                url: "",
+            },
+            {
+                name: "test",
+                url: "",
+            },
+        ],
+    },
+    {
+        name: "Top of Mew York",
+        url: "",
+        subMenu: false,
+        subLinks: [
+            {
+                name: "test",
+                url: "",
+            },
+            {
+                name: "test",
+                url: "",
+            },
+            {
+                name: "test",
+                url: "",
+            },
+        ],
+    },
+];

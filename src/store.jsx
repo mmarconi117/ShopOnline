90c77188f320910a18a000d1e3e4869141720695
jonathanj101 <jonatjimen@gmail.com>
2023-12-26 19:05:19 -0500

    updated file to use redux/toolkit

--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
-import rootReducer from './reducers'; // You need to create this file
+// import { createStore } from 'redux';
+// import rootReducer from './reducers'; // You need to create this file
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
+
+// export default store;
+
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers/index";
+
+const store = configureStore({ reducer: rootReducer });
 
 export default store;

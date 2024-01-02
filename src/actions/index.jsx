2c7adc306335c9f008893f7ebeb0a0cc179fcb7e
jonathanj101 <jonatjimen@gmail.com>
2024-01-01 23:45:28 -0500

    action types for single product and cart

---
+++ src/actions/index.jsx
@@ -0,0 +1,8 @@
+// Product details page
+export const SET_PRODUCT = "SET_PRODUCT";
+
+// cart actions
+
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_ITEM_FROM_CART = "REMOVE_ITEM_FROM_CART";
+export const UPDATE_CART = "UPDATE_CART";

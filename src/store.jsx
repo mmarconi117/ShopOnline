89b0c30ca8787195f2f1c9f08d382c96d1a31160
jonathanj101 <jonatjimen@gmail.com>
2024-01-02 23:11:24 -0500

    fixed merge conflict with main

--- src/store.jsx
+++ src/store.jsx
@@ -1,13 +1,6 @@
 import { createStore } from "redux";
 import rootReducer from "./reducersAndActions/reducers"; // Make sure this path is correct
 
-// const store = createStore(rootReducer);
-
-// export default store;
-
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers/index";
-
-const store = configureStore({ reducer: rootReducer });
+const store = createStore(rootReducer);
 
 export default store;

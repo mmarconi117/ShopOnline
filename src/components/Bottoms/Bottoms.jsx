aeda5b59ca9aab8cd28a374d2aa196df86b83780
Aaron Yii <aaronyii@mail.usf.edu>
2024-01-08 15:50:09 -0600

    Modified CSS codes on Subcategories page

--- src/components/Bottoms/Bottoms.jsx
+++ src/components/Bottoms/Bottoms.jsx
@@ -1,16 +1,16 @@
 import imagesList from "./BottomsRowImages";
 function BottomsRow() {
     return (
-        <div className="w-[95%] h-[445px] grid grid-cols-6 mx-auto gap-x-[26px] my-[50px]">
-            <h1 className="col-span-6 font-normal text-[31px] leading-[37.2px]">Bottoms</h1>
-            {imagesList.map((image) =>(
-
-                <div key={image.id} className="bg-white w-full h-auto px-[10px] pt-[10px] pb-[16px] rounded-[5px]">
-                    <img src={image.src} alt={image.alt} className="w-[199px] h-[222px] rounded-[5px]"/>
-                    <p className="text-[14px]">{image.description}</p>
-                </div>
-
-            ))}
+        <div className="w-full h-auto flex flex-col items-start gap-4 pt-8 px-4 sm:pt-16 sm:pb-1 sm:pl-10 sm:pr-0" >
+            <h1 className=" text-[#313133] font-Roboto font-medium text-base sm:text-[31px] sm:font-bold sm:leading-[37.2px]">Bottoms</h1>
+            <ul className="flex w-full items-start gap-2 sm:gap-6 overflow-x-auto">
+                {imagesList.map((image) =>(
+                    <li key={image.id} className="flex flex-col items-start bg-white min-w-[175px] min-h-[373px] sm:min-w-[219px] sm:h-auto p-2 pb-3 sm:p-[10px] sm:pb-[16px] rounded-[5px] border border-solid border-[#AEA9B1] gap-4 sm:gap-6">
+                        <img src={image.src} alt={image.alt} className="min-h-[214px] object-cover rounded-[5px]"/>
+                        <p className="font-Roboto text-sm max-sm:tracking-[0.25px]">{image.description}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }

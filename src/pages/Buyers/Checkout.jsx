67ebd139c976cd0efaf5ac6424ca179b586be428
yabetsg <yabets.g2@gmail.com>
2024-01-09 18:20:50 -0500

    Change edit to a button instead of a paragraph

--- src/pages/Buyers/Checkout.jsx
+++ src/pages/Buyers/Checkout.jsx
@@ -1,39 +1,38 @@
 const Checkout = () => {
   return (
     <div>
-        <div>
-            <p>Shipment Address</p>
-            <p>Edit</p>
-        </div>
+      <div>
+        <p>Shipment Address</p>
+        <button>Edit</button>
+      </div>
 
-        <p>Payment method</p>
+      <p>Payment method</p>
 
-        <p>Shipping method</p>
+      <p>Shipping method</p>
 
-        <div>
-            <label htmlFor="standard">Standard Shipping</label>
-            <input type="radio" name="shipping" id="" />
+      <div>
+        <label htmlFor="standard">Standard Shipping</label>
+        <input type="radio" name="shipping" id="" />
 
-            <label htmlFor="express">Express Shipping</label>
-            <input type="radio" name="shipping" id="" />
+        <label htmlFor="express">Express Shipping</label>
+        <input type="radio" name="shipping" id="" />
 
-            <label htmlFor="express">Overnight Shipping</label>
-            <input type="radio" name="shipping" id="" />
+        <label htmlFor="express">Overnight Shipping</label>
+        <input type="radio" name="shipping" id="" />
 
-            <button>Continue to payment</button>
-        </div>
+        <button>Continue to payment</button>
+      </div>
 
-
-        <div>
-            <p>Order summary:{}</p>
-            <p>Subtotal:{}</p>
-            <p>Promo code:{}</p>
-            <p>Shipping:{}</p>
-            <p>Estimated tax:{}</p>
-            <p>Total:{}</p>
-        </div>
+      <div>
+        <p>Order summary:{}</p>
+        <p>Subtotal:{}</p>
+        <p>Promo code:{}</p>
+        <p>Shipping:{}</p>
+        <p>Estimated tax:{}</p>
+        <p>Total:{}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;

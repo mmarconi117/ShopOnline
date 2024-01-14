56bcc3ed147bf2194470ae9c8bbf4a4133ea4937
Fish <kenandcrys@live.com>
2024-01-14 15:59:03 -0600

    update App.js, removed unused imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Libraries
-import { connect } from "react-redux";
-
-// Redux
-import { setCarts, addToCart } from "./reducersAndActions/actions/cartAction";
-
 // components
 import LandingPage from "./pages/Buyers/LandingPage";
 import SubCategory from "./pages/Buyers/SubCategory";
 import Cart from "./pages/Buyers/Cart/CartPage";
-import ProductDetails from "./pages/Buyers/Product-Details/ProductDetails";
+
 import LoginPage from "./pages/Buyers/LoginPage";
 import SignupPage from "./pages/Buyers/SignupPage";
 import Help from "./pages/Buyers/Help";
 import CompanyService from "./pages/Sellers/CompanyService";
 import Layout from "./Layout";
-import Homepage from "./pages/Sellers/Home/Homepage";
+
 import PaymentSuccessful from "./pages/Buyers/PaymentSuccessful";
 import Checkout from "./pages/Buyers/Checkout";
 

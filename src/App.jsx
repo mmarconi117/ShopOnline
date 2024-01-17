067965b2934469c886c477d74803f2deeac724d7
jonathanj101 <jonatjimen@gmail.com>
2024-01-16 20:09:29 -0500

    imported product catalogue and endpoint to access and view it

--- src/App.jsx
+++ src/App.jsx
@@ -18,35 +18,82 @@ import AccountSettings from "./pages/Buyers/Account";
 import Account from "./pages/Buyers/Account";
 import ReviewPage from "./pages/Buyers/ReviewPage";
 import LandingPageforSellers from "./pages/Sellers/LandingPageforSellers";
+import ProductCatalogue from "./pages/Buyers/Product-Catalogue/ProductCatalogue";
 
 function App() {
-
-  return (
-    <>
-      <div>
-        <Router>
-          <Layout> 
-            <Routes>
-              <Route exact path="/" element={<LandingPage />} />
-              <Route path="/subcategories" element={<SubCategory />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/sellers/*" element={<LandingPageforSellers />} />
-              <Route path="/help" element={<Help />} />
-              <Route path="/language" element="#" />
-              <Route path="/account" element={<Account/>} />
-              <Route path="/app" element="#" />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/companyService" element={<CompanyService/>}/>
-              <Route path="/paymentSuccess" element={<PaymentSuccessful/>}/>
-              <Route path="/checkout" element={<Checkout/>}/>
-              <Route path="/reviews" element={<ReviewPage/>} />
-            </Routes>
-          </Layout>
-        </Router>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Router>
+                    <Layout>
+                        <Routes>
+                            <Route
+                                exact
+                                path="/"
+                                element={<LandingPage />}
+                            />
+                            <Route
+                                path="/subcategories"
+                                element={<SubCategory />}
+                            />
+                            <Route
+                                path="/cart"
+                                element={<Cart />}
+                            />
+                            <Route
+                                path="/sellers/*"
+                                element={<LandingPageforSellers />}
+                            />
+                            <Route
+                                path="/product-catalogue"
+                                element={<ProductCatalogue />}
+                            />
+                            <Route
+                                path="/help"
+                                element={<Help />}
+                            />
+                            <Route
+                                path="/language"
+                                element="#"
+                            />
+                            <Route
+                                path="/account"
+                                element={<Account />}
+                            />
+                            <Route
+                                path="/app"
+                                element="#"
+                            />
+                            <Route
+                                path="/login"
+                                element={<LoginPage />}
+                            />
+                            <Route
+                                path="/signup"
+                                element={<SignupPage />}
+                            />
+                            <Route
+                                path="/companyService"
+                                element={<CompanyService />}
+                            />
+                            <Route
+                                path="/paymentSuccess"
+                                element={<PaymentSuccessful />}
+                            />
+                            <Route
+                                path="/checkout"
+                                element={<Checkout />}
+                            />
+                            <Route
+                                path="/reviews"
+                                element={<ReviewPage />}
+                            />
+                        </Routes>
+                    </Layout>
+                </Router>
+            </div>
+        </>
+    );
 }
 
 export default App;

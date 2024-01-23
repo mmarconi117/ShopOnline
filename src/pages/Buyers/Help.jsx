b53c2c0816615296bdc9bdacd411a31398fca2fd
Aaron Yii <aaronyii@mail.usf.edu>
2024-01-23 15:46:35 -0600

    Integrated CSS to help page from buyer side

--- src/pages/Buyers/Help.jsx
+++ src/pages/Buyers/Help.jsx
@@ -1,4 +1,3 @@
-import Footer from "../../components/Footer/Footer";
 import yellowchat from "../../assets/ICONS/yellowchat.svg"
 import notify from "../../assets/ICONS/notify.svg"
 import myaccout from "../../assets/ICONS/myaccout.svg"
@@ -9,148 +8,183 @@ import voucher from "../../assets/ICONS/voucher.svg"
 import saved from "../../assets/ICONS/voucher.svg"
 import Recently from "../../assets/ICONS/Recently.svg"
 import logoutred from "../../assets/ICONS/logoutred.svg"
+import arrowDown from "../../assets/ICONS/ArrowDown.svg"
 
 
 
 const Help = () => {
+  /*
+
+   */
   return (
-    <div>
-
-      <div className="self-stretch mt-20 mx-8 max-md:max-w-full max-md:mr-2.5 max-md:mt-10">
-        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-          <div className="flex flex-col items-stretch w-[24%] max-md:w-full max-md:ml-0">
-            <span className="items-stretch bg-white flex w-full grow flex-col mx-auto pt-5 pb-9 px-8 rounded-md max-md:mt-2.5 max-md:px-5">
-
-              <span className="items-stretch flex justify-between gap-3.5">
-                <img
-
-                  src={myaccout} alt=""
-                  className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
-                />
-                <div className="text-zinc-800 text-base font-medium leading-6">
-                  <a href="/account">My account</a>
-                </div>
-              </span>
-
-              <span className="items-stretch flex justify-between gap-3.5 mt-10">
-                <img src={order} alt="" />
-                <div className="text-zinc-800 text-base font-medium leading-6">
-                  <a href="">Orders</a>
-                </div>
-              </span>
-
-              <span className="items-stretch flex justify-between gap-3.5 mt-5">
-                <img src={Payonline} alt="" />
-                <div className="text-zinc-800 text-base font-medium leading-6">
-                  <a href="">Payments</a>
-                </div>
-              </span>
-              <span className="items-stretch flex justify-between gap-3.5 mt-5">
-                <img src={pending} alt="" />
-                <div className="text-zinc-800 text-base font-medium leading-6">
-                  <a href="">Pending Reviews</a>
-                </div>
-              </span>
-
-              <span className="items-stretch flex justify-between gap-3.5 mt-5">
-                <img src={voucher} alt="" />
-
-                <div className="text-zinc-800 text-base font-medium leading-6">
-                  <a href="">Voucher</a>
-                </div>
-              </span>
-
-              <span className="items-stretch flex justify-between gap-3.5 mt-5">
-                <img src={saved} alt="" />
-                <div className="text-zinc-800 text-base font-medium leading-6">
-                  <a href="">Saved items</a>
-                </div>
-              </span>
-
-              <span className="items-stretch flex justify-between gap-3.5 mt-5">
-                <img src={Recently} alt="" />
-                <div className="text-zinc-800 text-base font-medium leading-6">
-                  <a href="">Recently Viewed</a>
-                </div>
-              </span>
-              <span className="items-stretch flex justify-between gap-3.5 mt-5">
-                <img
-
-                  src={notify} alt=""
-                  className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
-                />
-                <div className="text-zinc-800 text-base font-medium leading-6">
-                  <a href="">Notifications</a>
-                </div>
-              </span>
-
-              <div className="text-zinc-800 text-base font-medium leading-6">
-                <a href="">Notifications</a>
-              </div>
-
-              <div className="text-zinc-800 text-base font-medium leading-6 mt-10">
-                <a href="">Account Management</a>
-              </div>
-              <div className="text-zinc-800 text-base font-medium leading-6">
-                <a href="">Edit Shipping address</a>
-              </div>
-              <div className="text-zinc-800 text-base font-medium leading-6">
-                <a href="">Digital service and device Support</a>
-              </div>
-              <div className="text-zinc-800 text-base font-medium leading-6 mt-5">
-                <a href="">Customer Service</a>
-              </div>
-              <span className="justify-between items-stretch flex gap-3.5 mt-10 px-20 max-md:px-5">
-                <img src={logoutred} alt="" />
-                <div className="text-red-400 text-base font-medium leading-6">LOGOUT</div>
-              </span>
-            </span>
-          </div>
-
-          <div className="flex flex-col items-stretch w-[76%] ml-5 max-md:w-full max-md:ml-0">
-            <span className="flex flex-col items-stretch max-md:max-w-full max-md:mt-2.5">
-              <div className="bg-white flex flex-col justify-center items-center px-16 py-8 max-md:max-w-full max-md:px-5">
-                <span className="flex w-[423px] max-w-full flex-col items-stretch">
-                  <span className="flex flex-col pl-6 pr-14 items-end max-md:max-w-full max-md:px-5">
-                    <div className="text-neutral-800 text-center text-2xl leading-8 whitespace-nowrap">
-                      Need help? Let us help you!
-                    </div>
-                    <div className="text-neutral-800 text-center text-2xl leading-8 self-stretch whitespace-nowrap mt-9">
-                      Give us a call: 1-800-123-4567
-                    </div>
-                  </span>
-                  <div className="text-neutral-800 text-center text-2xl leading-8 mt-2.5 max-md:max-w-full">
-                    Email us: nyshopping@shopping.com
-                  </div>
-                  <div className="text-neutral-800 text-center text-2xl leading-8 self-center whitespace-nowrap mt-1">
-                    Monday - Friday 6am - 5pm PST
-                  </div>
-                  <div className="text-neutral-800 text-center text-2xl leading-8 self-center whitespace-nowrap mt-1.5">
-                    Saturday - Sunday 8am - 2pm PST
-                  </div>
-                </span>
-              </div>
-              <div className="text-zinc-800 text-4xl font-medium leading-10 ml-4 mt-8 self-start max-md:max-w-full">
-                See frequently asked question on popular Topics
-              </div>
+    <div className="relative min-h-max w-full p-12 pt-[72px]">
+      <div className="hidden lg:block absolute w-[68px] right-0 bottom-[10px] lg:bottom-[50%]">
+        <img src={yellowchat} alt="chat-icon" className="object-contain shrink-0"/>
+      </div>
+      <div className="gap-5 flex flex-col lg:flex-row items-stretch lg:justify-center lg:items-start">
+        {/* Sidebar */}
+        <div className="bg-white flex flex-col gap-5 items-stretch min-h-max min-w-max w-full lg:w-[340px] px-8 pt-5 pb-9 grow-0">
+          <div className="flex items-stretch justify-start gap-4">
               <img
-
-                src={yellowchat} alt=""
-                className="aspect-[0.23] object-contain object-center w-16 shadow-sm overflow-hidden max-w-full mt-16 self-end max-md:mt-10"
+                src={myaccout} alt=""
+                className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
               />
-            </span>
+              <div className="text-zinc-800 text-base font-medium leading-6">
+                <a href="/account">My account</a>
+              </div>
+          </div>
+          <div className="w-full h-[0.5px] bg-gray-400"/>
+          <div className="flex items-stretch justify-start gap-4">
+              <img
+                src={order} alt="order-icon"
+                className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
+              />
+              <div className="text-zinc-800 text-base font-medium leading-6">
+                <a href="/account">Orders</a>
+              </div>
+          </div>
+          <div className="flex items-stretch justify-start gap-4">
+              <img
+                src={Payonline} alt="payment-icon"
+                className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
+              />
+              <div className="text-zinc-800 text-base font-medium leading-6">
+                <a href="/account">Payments</a>
+              </div>
+          </div>
+          <div className="flex items-stretch justify-start gap-4">
+              <img
+                src={pending} alt="reviews-icon"
+                className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
+              />
+              <div className="text-zinc-800 text-base font-medium leading-6">
+                <a href="/account">Pending Reviews</a>
+              </div>
+          </div>
+          <div className="flex items-stretch justify-start gap-4">
+              <img
+                src={voucher} alt="voucher-icon"
+                className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
+              />
+              <div className="text-zinc-800 text-base font-medium leading-6">
+                <a href="/account">Voucher</a>
+              </div>
+          </div>
+          <div className="flex items-stretch justify-start gap-4">
+              <img
+                src={saved} alt="saved-items icon"
+                className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
+              />
+              <div className="text-zinc-800 text-base font-medium leading-6">
+                <a href="/account">Saved Items</a>
+              </div>
+          </div>
+          <div className="flex items-stretch justify-start gap-4">
+              <img
+                src={Recently} alt="recently-viewed icon"
+                className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
+              />
+              <div className="text-zinc-800 text-base font-medium leading-6">
+                <a href="/account">Recently Viewed</a>
+              </div>
+          </div>
+          <div className="flex items-stretch justify-start gap-4">
+              <img
+                src={notify} alt="notification icon"
+                className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full"
+              />
+              <div className="text-zinc-800 text-base font-medium leading-6">
+                <a href="/account">Notifications</a>
+              </div>
+          </div>
+          <div className="w-full h-[0.5px] bg-gray-400"/>
+
+          <div className="text-zinc-800 text-base font-medium leading-6">
+            <a href="">Account Management</a>
+          </div>
+          <div className="text-zinc-800 text-base font-medium leading-6">
+            <a href="">Edit Shipping address</a>
+          </div>
+          <div className="text-zinc-800 text-base font-medium leading-6">
+            <a href="">Digital service and device Support</a>
+          </div>
+          <div className="text-zinc-800 text-base font-medium leading-6">
+            <a href="">Customer Service</a>
+          </div>
+          <div className="w-full h-[0.5px] bg-gray-400"/>
+          <div className="justify-between items-stretch flex gap-3.5 mx-auto">
+            <img src={logoutred} alt="logout icon" />
+            <div className="text-red-400 text-base font-medium leading-6">LOGOUT</div>
+          </div>
+        </div>
+        {/* Content */}
+        <div className="flex flex-col items-stretch lg:w-full grow gap-7">
+          {/* Top part outer container */}
+          <div className="bg-white flex flex-col justify-center items-center px-16 py-8 gap-7">
+            <div className="text-neutral-800 text-center text-lg sm:text-2xl leading-8 whitespace-nowrap">
+              Need help? Let us help you!
+            </div>
+            <div className="flex flex-col items-stretch">
+              <div className="text-neutral-800 text-center text-base sm:text-2xl leading-8 self-stretch whitespace-nowrap">
+                Give us a call: 1-800-123-4567
+              </div>
+              <div className="text-neutral-800 text-center text-base sm:text-2xl leading-8 max-md:max-w-full">
+                Email us: nyshopping@shopping.com
+              </div>
+              <div className="text-neutral-800 text-center text-base sm:text-2xl leading-8 self-center whitespace-nowrap">
+              Monday - Friday 6am - 5pm PST
+              </div>
+              <div className="text-neutral-800 text-center text-base sm:text-2xl leading-8 self-center whitespace-nowrap">
+              Saturday - Sunday 8am - 2pm PST
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-8 font-Roboto text-zinc-800 max-w-[1000px] lg:pr-[72px]">
+            <div className="text-xl lg:text-2xl 2xl:text-4xl font-medium leading-10">See frequently asked question on popular Topics</div>
+            <div className="flex flex-col gap-10">
+              <div className="flex justify-between">
+                <div className="pr-8">Frequently asked question on Account</div>
+                <div className=" min-w-4">
+                  <img src={arrowDown} alt="arrow-icon"  className="object-contain"/>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <div className="pr-8">Frequently asked question on Payments & Refunds</div>
+                <div className=" min-w-4">
+                  <img src={arrowDown} alt="arrow-icon"  className="object-contain"/>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <div className="pr-8">Frequently asked question on Orders, Shipping & Tracking</div>
+                <div className=" min-w-4">
+                  <img src={arrowDown} alt="arrow-icon"  className="object-contain"/>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <div className="pr-8">Frequently asked question on Product</div>
+                <div className=" min-w-4">
+                  <img src={arrowDown} alt="arrow-icon"  className="object-contain"/>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <div className="pr-8">Frequently asked question on Returns & Exchanges</div>
+                <div className=" min-w-4">
+                  <img src={arrowDown} alt="arrow-icon"  className="object-contain"/>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <div className="pr-8">Frequently asked question on Marketing Inquiries</div>
+                <div className=" min-w-4">
+                  <img src={arrowDown} alt="arrow-icon"  className="object-contain"/>
+                </div>
+              </div>
+            </div>
+            <button className="self-center font-Roboto border border-solid rounded-[5px] border-[#2284B6] text-[#2284B6] text-base sm:text-xl sm:leading-6 px-6 py-3 sm:px-8 sm:py-4">Give us feedback</button>
           </div>
         </div>
       </div>
-
-
-      <button className="text-cyan-600 text-center text-xl leading-6 whitespace-nowrap justify-center items-stretch border border-[color:var(--Color-Styles-Primary-color-blue-T1000,#2284B6)] self-center ml-44 mt-28 px-12 py-4 rounded-md border-solid max-md:mt-10 max-md:px-5">
-        Give us feedback
-      </button>
-
-
-      <Footer />
     </div>
+
   );
 };
 

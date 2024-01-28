750c009724ab1417abb30ca79fcdba463ec7024e
jonathanj101 <jonatjimen@gmail.com>
2024-01-27 23:50:20 -0500

    separeted buyers and sellers components into it's own group, for readability purposes

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,32 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // components
+// buyers
 import LandingPage from "./pages/Buyers/LandingPage";
 import SubCategory from "./pages/Buyers/SubCategory";
 import Cart from "./pages/Buyers/Cart/CartPage";
-
 import LoginPage from "./pages/Buyers/LoginPage";
 import SignupPage from "./pages/Buyers/SignupPage";
 import Help from "./pages/Buyers/Help";
-import CompanyService from "./pages/Sellers/CompanyService";
-import Layout from "./Layout";
-
 import PaymentSuccessful from "./pages/Buyers/PaymentSuccessful";
 import Checkout from "./pages/Buyers/Checkout";
 import Account from "./pages/Buyers/Account";
 import ReviewPage from "./pages/Buyers/ReviewPage";
+import ProductDetails from "./pages/Buyers/Product-Details/ProductDetails";
+import ProductCatalogue from "./pages/Buyers/Product-Catalogue/ProductCatalogue";
+
+// layout
+import Layout from "./Layout";
+
+// Sellers
+import CompanyService from "./pages/Sellers/CompanyService";
 import LandingPageforSellers from "./pages/Sellers/LandingPageforSellers";
 import CompanyInfo from "./pages/Sellers/CompanyInfo/CompanyInfo";
 import OnlineFulfillment from "./pages/Sellers/OnlineFulfillment/OnlineFulfillment";
-import ProductDetails from "./pages/Buyers/Product-Details/ProductDetails";
 import PaymentServices from "./pages/Sellers/PaymentServices/PaymentServices";
 import TwoStepVerification from "./pages/Sellers/2-StepVerification/TwoStepVerification";
 import Agreements from "./pages/Sellers/Agreements/Agreements";
-import ProductCatalogue from "./pages/Buyers/Product-Catalogue/ProductCatalogue";
+
 function App() {
     
   return (

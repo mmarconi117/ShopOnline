b4c149e137cd0d91954bed25edea17e693088ca3
Aaron Yii <aaronyii@mail.usf.edu>
2024-01-30 15:22:36 -0600

    Added CSS to Homepage from seller side

--- src/pages/Sellers/Home/Homepage.jsx
+++ src/pages/Sellers/Home/Homepage.jsx
@@ -10,9 +10,9 @@ function Homepage() {
     const [showModal, setShowModal] = useState(false);
 
     return (
-        <div className="self-center flex w-[967px] max-w-full flex-col items-stretch mt-4 px-5">
+        <div className="self-center flex max-w-full flex-col items-stretch p-10 pb-20 gap-10">
             {showModal && <GetVerified setShowModal={setShowModal} />}
-            <div className="items-stretch flex justify-between gap-5 mt-5 max-md:max-w-full max-md:flex-wrap">
+            <div className="flex flex-col justify-start items-start gap-2 sm:gap-0 sm:flex-row sm:justify-between sm:items-center max-w-full ">
                 <div className="text-zinc-800 text-base font-semibold leading-6 whitespace-nowrap">
                     Follow these 3 easy steps to start selling!
                 </div>
@@ -21,72 +21,64 @@ function Homepage() {
                 </div>
             </div>
 
-            <div className="mt-4 max-md:max-w-full">
-                <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-                    <div className="flex flex-col items-center w-[33%] max-md:w-full max-md:ml-0">
-                        <div className="bg-white flex grow gap-3 w-full pl-3 pr-16 py-4 rounded-md items-start max-md:mt-10 max-md:pr-5">
-                            <p className="text-black text-left text-base leading-6 grow whitespace-nowrap">
-                                1.
-                            </p>
-                            <div className=" self-stretch flex grow basis-[0%] flex-col">
-                                <img
-                                    loading="lazy"
-                                    src={GetVerifiedIcon}
-                                    className="aspect-square object-contain object-center w-12 overflow-hidden self-center max-w-full"
-                                />
-                                <p className="text-zinc-700 text-center text-[16px] leading-6 mt-6">
-                                    Add your business info to complete our quick-check verification
-                                </p>
-
-                                <button
-                                    className="text-zinc-700 text-center text-sm font-semibold leading-6 whitespace-nowrap items-stretch border-[color:var(--color-styles-neutral-600,#79767D)] self-center justify-center mt-6 px-8 py-2 rounded-[30px] border-[0.75px] border-solid max-md:px-5"
-                                    onClick={() => setShowModal(true)}
-                                >
-                                    Get verified
-                                </button>
-
-                            </div>
-                        </div>
+            <div className="max-w-full flex justify-between items-stretch gap-5">
+                <div className="min-w-[290px] bg-white flex grow gap-3 px-5 py-4 rounded-md items-stretch">
+                    <div className="max-w-max text-black text-left text-base leading-6 grow-0">
+                        1.
                     </div>
-                    <div className="flex flex-col items-center w-[34%] max-md:w-full max-md:ml-0">
-                        <div className="bg-white flex grow gap-3 w-full pl-3 pr-16 py-4 rounded-md items-start max-md:mt-10 max-md:pr-5">
-                            <p className="text-black text-left text-base leading-6 grow whitespace-nowrap">
-                                2.
-                            </p>
-                            <div className=" self-stretch flex grow basis-[0%] flex-col">
-                                <img
-                                    loading="lazy"
-                                    src={Paid}
-                                    className="aspect-square object-contain object-center w-12 overflow-hidden self-center max-w-full"
-                                />
-                                <p className="text-zinc-700 text-center text-[16px] leading-6 mt-6">
-                                    Tell us where to send your payments. No setup, subscription or monthly fees.
-                                </p>
-                                <button className="text-zinc-700 text-center text-sm font-semibold leading-6 whitespace-nowrap items-stretch border-[color:var(--color-styles-neutral-600,#79767D)] self-center justify-center mt-6 px-8 py-2 rounded-[30px] border-[0.75px] border-solid max-md:px-5">
-                                    Get paid
-                                </button>
-                            </div>
-                        </div>
+                    <div className="flex grow flex-col justify-between items-center gap-3 sm:gap-6">
+                        <img
+                            loading="lazy"
+                            src={GetVerifiedIcon}
+                            className="aspect-square object-contain object-center w-12 overflow-hidden self-center max-w-full"
+                        />
+                        <p className="text-zinc-700 text-center text-[16px] leading-6 mt-6">
+                            Add your business info to complete our quick-check verification
+                        </p>
+
+                        <button
+                            className="text-zinc-700 text-center text-sm font-semibold leading-6 whitespace-nowrap items-stretch border-[color:var(--color-styles-neutral-600,#79767D)] self-center justify-center px-8 py-2 rounded-[30px] border-[0.75px] border-solid max-md:px-5"
+                            onClick={() => setShowModal(true)}
+                        >
+                            Get verified
+                        </button>
+
                     </div>
-                    <div className="flex flex-col items-center w-[33%] max-md:w-full max-md:ml-0">
-                        <div className="bg-white flex grow gap-3 w-full pl-3 pr-16 py-4 rounded-md items-start max-md:mt-10 max-md:pr-5">
-                            <p className="text-black text-left text-base leading-6 grow whitespace-nowrap">
-                                3.
-                            </p>
-                            <div className=" self-stretch flex grow basis-[0%] flex-col">
-                                <img
-                                    loading="lazy"
-                                    src={Shipping}
-                                    className="aspect-square object-contain object-center w-12 overflow-hidden self-center max-w-full"
-                                />
-                                <p className="text-zinc-700 text-center text-[16px] leading-6 mt-6">
-                                    Select the best shipping price and partners for your budget.
-                                </p>
-                                <button className="text-zinc-700 text-center text-sm font-semibold leading-6 whitespace-nowrap items-stretch border-[color:var(--color-styles-neutral-600,#79767D)] self-center justify-center mt-6 px-8 py-2 rounded-[30px] border-[0.75px] border-solid max-md:px-5">
-                                    Start shipping
-                                </button>
-                            </div>
-                        </div>
+                </div>
+                <div className="min-w-[290px] bg-white flex grow gap-3 px-5 py-4 rounded-md items-stretch">
+                    <div className="max-w-max text-black text-left text-base leading-6 grow-0">
+                        2.
+                    </div>
+                    <div className="flex grow flex-col justify-between items-center gap-3 sm:gap-6">
+                        <img
+                            loading="lazy"
+                            src={Paid}
+                            className="aspect-square object-contain object-center w-12 overflow-hidden self-center max-w-full"
+                        />
+                        <p className="text-zinc-700 text-center text-[16px] leading-6 mt-6">
+                            Tell us where to send your payments. No setup, subscription or monthly fees.
+                        </p>
+                        <button className="text-zinc-700 text-center text-sm font-semibold leading-6 whitespace-nowrap items-stretch border-[color:var(--color-styles-neutral-600,#79767D)] self-center justify-center mt-6 px-8 py-2 rounded-[30px] border-[0.75px] border-solid max-md:px-5">
+                            Get paid
+                        </button>
+                    </div>
+                </div>
+                <div className="min-w-[290px] bg-white flex grow gap-3 px-5 py-4 rounded-md items-stretch">
+                    <div className="max-w-max text-black text-left text-base leading-6 grow-0">
+                        3.
+                    </div>
+                    <div className="flex grow flex-col justify-between items-center gap-3 sm:gap-6">
+                        <img
+                            loading="lazy"
+                            src={Shipping}
+                            className="aspect-square object-contain object-center w-12 overflow-hidden self-center max-w-full"
+                        />
+                        <p className="text-zinc-700 text-center text-[16px] leading-6 mt-6">
+                            Select the best shipping price and partners for your budget.
+                        </p>
+                        <button className="text-zinc-700 text-center text-sm font-semibold leading-6 whitespace-nowrap items-stretch border-[color:var(--color-styles-neutral-600,#79767D)] self-center justify-center mt-6 px-8 py-2 rounded-[30px] border-[0.75px] border-solid max-md:px-5">
+                            Start shipping
+                        </button>
                     </div>
                 </div>
             </div>

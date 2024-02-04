1747c0a9d413a7726c458b3f819ddd32353d660a
jonathanj101 <jonatjimen@gmail.com>
2024-02-04 18:57:12 -0500

    route for product catalogue

--- src/App.jsx
+++ src/App.jsx
@@ -35,36 +35,112 @@ function App() {
   return (
       <div>
           <Router>
-            <Routes>
-              <Route exact path="" element={<Layout />}>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/subcategories" element={<SubCategory />} />
-                <Route path="/productdetails" element={<ProductDetails />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/help" element={<Help />} />
-                <Route path="/language" element="#" />
-                <Route path="/account" element={<Account/>} />
-                <Route path="/app" element="#" />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/companyService" element={<CompanyService/>}/>
-                <Route path="/paymentSuccess" element={<PaymentSuccessful/>}/>
-                <Route path="/checkout" element={<Checkout/>}/>
-                <Route path="/reviews" element={<ReviewPage/>} />
-                <Route path="/accountsettings" element={<Account />} />
-                <Route path="/ratings-reviews" element={<RatingsAndReviews />} />
-              </Route>
+              <Routes>
+                  <Route
+                      exact
+                      path=""
+                      element={<Layout />}
+                  >
+                      <Route
+                          path="/"
+                          element={<LandingPage />}
+                      />
+                      <Route
+                          path="/subcategories"
+                          element={<SubCategory />}
+                      />
+                      <Route
+                          path="/productcatalogue"
+                          element={<ProductCatalogue />}
+                      />
+                      <Route
+                          path="/productdetails"
+                          element={<ProductDetails />}
+                      />
+                      <Route
+                          path="/cart"
+                          element={<Cart />}
+                      />
+                      <Route
+                          path="/help"
+                          element={<Help />}
+                      />
+                      <Route
+                          path="/language"
+                          element="#"
+                      />
+                      <Route
+                          path="/account"
+                          element={<Account />}
+                      />
+                      <Route
+                          path="/app"
+                          element="#"
+                      />
+                      <Route
+                          path="/login"
+                          element={<LoginPage />}
+                      />
+                      <Route
+                          path="/signup"
+                          element={<SignupPage />}
+                      />
+                      <Route
+                          path="/companyService"
+                          element={<CompanyService />}
+                      />
+                      <Route
+                          path="/paymentSuccess"
+                          element={<PaymentSuccessful />}
+                      />
+                      <Route
+                          path="/checkout"
+                          element={<Checkout />}
+                      />
+                      <Route
+                          path="/reviews"
+                          element={<ReviewPage />}
+                      />
+                      <Route
+                          path="/accountsettings"
+                          element={<Account />}
+                      />
+                      <Route
+                          path="/ratings-reviews"
+                          element={<RatingsAndReviews />}
+                      />
+                  </Route>
 
-
-              <Route path="/sellers/*" element={<LandingPageforSellers />} />
-              <Route path="/onlinefulfillment" element={<OnlineFulfillment />} />
-              <Route path="/companyinfo" element={<CompanyInfo />} />
-              <Route path="/paymentservices" element={<PaymentServices />} />
-              <Route path="/twostepverification" element={<TwoStepVerification />} />
-              <Route path="/agreements" element={<Agreements />} />
-              <Route path="/notificationsettings" element={<NotificationSettings />} />
-            </Routes>
-        </Router>
+                  <Route
+                      path="/sellers/*"
+                      element={<LandingPageforSellers />}
+                  />
+                  <Route
+                      path="/onlinefulfillment"
+                      element={<OnlineFulfillment />}
+                  />
+                  <Route
+                      path="/companyinfo"
+                      element={<CompanyInfo />}
+                  />
+                  <Route
+                      path="/paymentservices"
+                      element={<PaymentServices />}
+                  />
+                  <Route
+                      path="/twostepverification"
+                      element={<TwoStepVerification />}
+                  />
+                  <Route
+                      path="/agreements"
+                      element={<Agreements />}
+                  />
+                  <Route
+                      path="/notificationsettings"
+                      element={<NotificationSettings />}
+                  />
+              </Routes>
+          </Router>
       </div>
   );
 }

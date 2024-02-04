7b3c9541c4ceaf85878847c5142d1f3478e28424
jonathanj101 <jonatjimen@gmail.com>
2024-02-04 18:59:10 -0500

    implemented search functionalities

--- src/pages/Sellers/OrderManagement/Returns-And-Refunds/Components/Search-bar/SearchBar.jsx
+++ src/pages/Sellers/OrderManagement/Returns-And-Refunds/Components/Search-bar/SearchBar.jsx
@@ -1,4 +1,21 @@
-const SearchBar = () => {
+const SearchBar = ({ returns, returnsCopy }) => {
+    const onChange = (e) => {
+        const userInput = e;
+
+        let update = [];
+
+        returns.forEach((item) => {
+            if (
+                userInput == item.orderNumber ||
+                userInput == item.purchaseOrderNumber ||
+                userInput == item.rmaNumber ||
+                userInput == item.customerOrderNumber
+            ) {
+                update.push(item);
+            }
+        });
+    };
+
     return (
         <div id="search-bar-component">
             <div className="flex w-full border-2 p-3 rounded-md">
@@ -7,19 +24,20 @@ const SearchBar = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="1.5"
+                        strokeWidth="1.5"
                         stroke="currentColor"
-                        class="w-6 h-6"
+                        className="w-6 h-6"
                     >
                         <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                         />
                     </svg>
                 </div>
                 <div className="w-full">
                     <input
+                        onChange={(e) => onChange(parseFloat(e.target.value))}
                         className="w-full bg-transparent focus:outline-none"
                         type="text"
                         placeholder="Search Purchase Order #, RMA #, Customer Order #"

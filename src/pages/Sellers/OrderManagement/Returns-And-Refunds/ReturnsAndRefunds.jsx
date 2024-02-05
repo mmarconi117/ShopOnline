63c4eff2d47351e13b762e73b1c03da7b972b9a1
jonathanj101 <jonatjimen@gmail.com>
2024-02-04 19:00:57 -0500

    imported download/filter components, passing props to children components and implemented functioanlities

--- src/pages/Sellers/OrderManagement/Returns-And-Refunds/ReturnsAndRefunds.jsx
+++ src/pages/Sellers/OrderManagement/Returns-And-Refunds/ReturnsAndRefunds.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
+import Download from "./Components/Download/Download";
 import Feedback from "./Components/Feedback/Feedback";
+import FiltersComponent from "./Components/Filter/FiltersComponent";
 import Completed from "./Components/ReturnsOverview/Completed";
 import Initiated from "./Components/ReturnsOverview/Initiated";
 import Received from "./Components/ReturnsOverview/Received";
@@ -6,6 +9,72 @@ import SearchBar from "./Components/Search-bar/SearchBar";
 import Table from "./Components/Table/Table";
 
 function ReturnsAndRefunds() {
+    const [returns, setReturns] = useState([
+        {
+            orderNumber: 15,
+            purchaseOrderNumber: 6,
+            rmaNumber: 11,
+            customerOrderNumber: 17,
+            price: 2222,
+            status: 27,
+            selected: false
+        },
+        {
+            orderNumber: 2,
+            purchaseOrderNumber: 7,
+            rmaNumber: 12,
+            customerOrderNumber: 18,
+            price: 232,
+            status: 28,
+            selected: false
+        },
+        {
+            orderNumber: 3,
+            purchaseOrderNumber: 8,
+            rmaNumber: 13,
+            customerOrderNumber: 19,
+            price: 242,
+            status: 29,
+            selected: false
+        },
+        {
+            orderNumber: 4,
+            purchaseOrderNumber: 9,
+            rmaNumber: 14,
+            customerOrderNumber: 20,
+            price: 252,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 5,
+            purchaseOrderNumber: 10,
+            rmaNumber: 15,
+            customerOrderNumber: 21,
+            price: 262,
+            status: 30,
+            selected: false
+        }
+    ]);
+    const [returnsCopy, setReturnsCopy] = useState([]);
+    const [itemIndex, setItemIndex] = useState(undefined); // selected item index
+
+    useEffect(() => {
+        setReturnsCopy(returns);
+    }, []);
+
+    const updateCopy = (update) => {
+        setReturnsCopy(update);
+    };
+
+    const resetFilter = (returns) => {
+        setReturnsCopy(returns);
+    };
+    // function to update download component
+    const getItemSelected = (itemIndex) => {
+        setItemIndex(itemIndex);
+    };
+
     return (
         <div className="self-center flex w-full max-w-full flex-col items-stretch p-5">
             <div className="flex">
@@ -17,7 +86,10 @@ function ReturnsAndRefunds() {
                 </div>
             </div>
             <div>
-                <SearchBar />
+                <SearchBar
+                    returns={returns}
+                    returnsCopy={returnsCopy}
+                />
             </div>
             <div className="flex justify-center my-5">
                 <Initiated />
@@ -25,7 +97,24 @@ function ReturnsAndRefunds() {
                 <Completed />
             </div>
             <div>
-                <Table />
+                <FiltersComponent
+                    returns={returns}
+                    returnsCopy={returnsCopy}
+                    resetFilter={resetFilter}
+                    updateCopy={updateCopy}
+                />
+                <Download
+                    returnsCopy={returnsCopy}
+                    itemIndex={itemIndex}
+                />
+            </div>
+            <div>
+                <Table
+                    returns={returns}
+                    returnsCopy={returnsCopy}
+                    updateCopy={updateCopy}
+                    getItemSelected={getItemSelected}
+                />
             </div>
             <div className="my-5">
                 <Feedback />

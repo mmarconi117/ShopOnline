0ab382cc8ab27cb39111486078c0dd43f0f9b544
jonathanj101 <jonatjimen@gmail.com>
2024-02-04 20:35:25 -0500

    added more returns items to test pagination

--- src/pages/Sellers/OrderManagement/Returns-And-Refunds/ReturnsAndRefunds.jsx
+++ src/pages/Sellers/OrderManagement/Returns-And-Refunds/ReturnsAndRefunds.jsx
@@ -54,14 +54,235 @@ function ReturnsAndRefunds() {
             price: 262,
             status: 30,
             selected: false
+        },
+        {
+            orderNumber: 15,
+            purchaseOrderNumber: 6,
+            rmaNumber: 11,
+            customerOrderNumber: 17,
+            price: 2222,
+            status: 27,
+            selected: false
+        },
+        {
+            orderNumber: 2,
+            purchaseOrderNumber: 7,
+            rmaNumber: 12,
+            customerOrderNumber: 18,
+            price: 232,
+            status: 28,
+            selected: false
+        },
+        {
+            orderNumber: 3,
+            purchaseOrderNumber: 8,
+            rmaNumber: 13,
+            customerOrderNumber: 19,
+            price: 242,
+            status: 29,
+            selected: false
+        },
+        {
+            orderNumber: 4,
+            purchaseOrderNumber: 9,
+            rmaNumber: 14,
+            customerOrderNumber: 20,
+            price: 252,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 5,
+            purchaseOrderNumber: 10,
+            rmaNumber: 15,
+            customerOrderNumber: 21,
+            price: 262,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 15,
+            purchaseOrderNumber: 6,
+            rmaNumber: 11,
+            customerOrderNumber: 17,
+            price: 2222,
+            status: 27,
+            selected: false
+        },
+        {
+            orderNumber: 2,
+            purchaseOrderNumber: 7,
+            rmaNumber: 12,
+            customerOrderNumber: 18,
+            price: 232,
+            status: 28,
+            selected: false
+        },
+        {
+            orderNumber: 3,
+            purchaseOrderNumber: 8,
+            rmaNumber: 13,
+            customerOrderNumber: 19,
+            price: 242,
+            status: 29,
+            selected: false
+        },
+        {
+            orderNumber: 4,
+            purchaseOrderNumber: 9,
+            rmaNumber: 14,
+            customerOrderNumber: 20,
+            price: 252,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 5,
+            purchaseOrderNumber: 10,
+            rmaNumber: 15,
+            customerOrderNumber: 21,
+            price: 262,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 15,
+            purchaseOrderNumber: 6,
+            rmaNumber: 11,
+            customerOrderNumber: 17,
+            price: 2222,
+            status: 27,
+            selected: false
+        },
+        {
+            orderNumber: 2,
+            purchaseOrderNumber: 7,
+            rmaNumber: 12,
+            customerOrderNumber: 18,
+            price: 232,
+            status: 28,
+            selected: false
+        },
+        {
+            orderNumber: 3,
+            purchaseOrderNumber: 8,
+            rmaNumber: 13,
+            customerOrderNumber: 19,
+            price: 242,
+            status: 29,
+            selected: false
+        },
+        {
+            orderNumber: 4,
+            purchaseOrderNumber: 9,
+            rmaNumber: 14,
+            customerOrderNumber: 20,
+            price: 252,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 5,
+            purchaseOrderNumber: 10,
+            rmaNumber: 15,
+            customerOrderNumber: 21,
+            price: 262,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 15,
+            purchaseOrderNumber: 6,
+            rmaNumber: 11,
+            customerOrderNumber: 17,
+            price: 2222,
+            status: 27,
+            selected: false
+        },
+        {
+            orderNumber: 2,
+            purchaseOrderNumber: 7,
+            rmaNumber: 12,
+            customerOrderNumber: 18,
+            price: 232,
+            status: 28,
+            selected: false
+        },
+        {
+            orderNumber: 3,
+            purchaseOrderNumber: 8,
+            rmaNumber: 13,
+            customerOrderNumber: 19,
+            price: 242,
+            status: 29,
+            selected: false
+        },
+        {
+            orderNumber: 4,
+            purchaseOrderNumber: 9,
+            rmaNumber: 14,
+            customerOrderNumber: 20,
+            price: 252,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 5,
+            purchaseOrderNumber: 10,
+            rmaNumber: 15,
+            customerOrderNumber: 21,
+            price: 262,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 15,
+            purchaseOrderNumber: 6,
+            rmaNumber: 11,
+            customerOrderNumber: 17,
+            price: 2222,
+            status: 27,
+            selected: false
+        },
+        {
+            orderNumber: 2,
+            purchaseOrderNumber: 7,
+            rmaNumber: 12,
+            customerOrderNumber: 18,
+            price: 232,
+            status: 28,
+            selected: false
+        },
+        {
+            orderNumber: 3,
+            purchaseOrderNumber: 8,
+            rmaNumber: 13,
+            customerOrderNumber: 19,
+            price: 242,
+            status: 29,
+            selected: false
+        },
+        {
+            orderNumber: 4,
+            purchaseOrderNumber: 9,
+            rmaNumber: 14,
+            customerOrderNumber: 20,
+            price: 252,
+            status: 30,
+            selected: false
+        },
+        {
+            orderNumber: 5,
+            purchaseOrderNumber: 10,
+            rmaNumber: 15,
+            customerOrderNumber: 21,
+            price: 262,
+            status: 30,
+            selected: false
         }
     ]);
     const [returnsCopy, setReturnsCopy] = useState([]);
     const [itemIndex, setItemIndex] = useState(undefined); // selected item index
-
-    useEffect(() => {
-        setReturnsCopy(returns);
-    }, []);
 
     const updateCopy = (update) => {
         setReturnsCopy(update);

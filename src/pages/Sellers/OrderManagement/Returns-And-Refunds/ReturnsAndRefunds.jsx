bfea7e8982a574857964f4e45fe32040a82edd29
jonathanj101 <jonatjimen@gmail.com>
2024-02-09 13:10:10 -0500

    implemented redux logic and update children components with props

--- src/pages/Sellers/OrderManagement/Returns-And-Refunds/ReturnsAndRefunds.jsx
+++ src/pages/Sellers/OrderManagement/Returns-And-Refunds/ReturnsAndRefunds.jsx
@@ -1,7 +1,8 @@
 // Libs
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+
 // Components
 import Download from "./Components/Download/Download";
 import Feedback from "./Components/Feedback/Feedback";
@@ -12,26 +13,48 @@ import Received from "./Components/ReturnsOverview/Received";
 import SearchBar from "./Components/Search-bar/SearchBar";
 import Table from "./Components/Table/Table";
 
-const mapStateToProps = (state) => ({
-    returns: state.returnsReducers.returns
+// redux
+import {
+    searchDisputes,
+    toggleFilterDisputes
+} from "../../../../reducersAndActions/actions/disputeAction";
+
+const mapStateToProps = (state) => {
+    return {
+        disputes: state.disputeReducer.disputes,
+        filtered: state.disputeReducer.filtered
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    searchDisputes: (data) => dispatch(searchDisputes(data)),
+    toggleFilterDisputes: (data) => dispatch(toggleFilterDisputes(data))
 });
 
-function ReturnsAndRefunds({ returns }) {
-    const [returnsCopy, setReturnsCopy] = useState([]);
+function ReturnsAndRefunds({ disputes, searchDisputes, filtered, toggleFilterDisputes }) {
+    const [disputesCopy, setDisputesCopy] = useState([]);
     const [itemIndex, setItemIndex] = useState(undefined); // selected item index, one single item
+    const [isResetPagination, setResetPagination] = useState(false);
+
+    useEffect(() => {
+        disputes.forEach((item) => (item.selected = false));
+    }, []);
 
     const updateCopy = (update) => {
-        setReturnsCopy(update);
+        setDisputesCopy(update);
     };
 
-    const resetFilter = (returns) => {
-        setReturnsCopy(returns);
+    const resetFilter = (update) => {
+        setDisputesCopy(update);
     };
     // function to update download component
     const getItemSelected = (itemIndex) => {
         setItemIndex(itemIndex);
     };
 
+    const resetPagination = () => {
+        setResetPagination(!isResetPagination);
+    };
     return (
         <div className="self-center flex w-full max-w-full flex-col items-stretch p-5">
             <div className="flex">
@@ -44,8 +67,12 @@ function ReturnsAndRefunds({ returns }) {
             </div>
             <div>
                 <SearchBar
-                    returns={returns}
-                    returnsCopy={returnsCopy}
+                    disputes={disputes}
+                    searchDisputes={searchDisputes}
+                    disputesCopy={disputesCopy}
+                    updateCopy={updateCopy}
+                    filtered={filtered}
+                    resetPagination={resetPagination}
                 />
             </div>
             <div className="flex justify-center my-5">
@@ -55,22 +82,27 @@ function ReturnsAndRefunds({ returns }) {
             </div>
             <div>
                 <FiltersComponent
-                    returns={returns}
-                    returnsCopy={returnsCopy}
+                    disputes={disputes}
+                    disputesFiltered={filtered}
+                    disputesCopy={disputesCopy}
                     resetFilter={resetFilter}
                     updateCopy={updateCopy}
+                    resetPagination={resetPagination}
+                    toggleFilterDisputes={toggleFilterDisputes}
                 />
                 <Download
-                    returnsCopy={returnsCopy}
+                    disputes={disputes}
                     itemIndex={itemIndex}
                 />
             </div>
             <div>
                 <Table
-                    returns={returns}
-                    returnsCopy={returnsCopy}
+                    disputes={disputes}
+                    disputesCopy={disputesCopy}
                     updateCopy={updateCopy}
                     getItemSelected={getItemSelected}
+                    resetPagination={resetPagination}
+                    isResetPagination={isResetPagination}
                 />
             </div>
             <div className="my-5">
@@ -81,7 +113,7 @@ function ReturnsAndRefunds({ returns }) {
 }
 
 ReturnsAndRefunds.propTypes = {
-    returns: PropTypes.array.isRequired
+    disputes: PropTypes.array.isRequired
 };
 
-export default connect(mapStateToProps, {})(ReturnsAndRefunds);
+export default connect(mapStateToProps, mapDispatchToProps)(ReturnsAndRefunds);

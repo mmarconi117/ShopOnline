90933f5c4fe41473f1affbd048481655a9e4d3f8
Nathan <nathan.forman@gmail.com>
2024-02-08 16:00:06 -0800

    Got inputs to display col below sm breakpoint

--- src/pages/Sellers/ProductCatalogue/UploadProduct.jsx
+++ src/pages/Sellers/ProductCatalogue/UploadProduct.jsx
@@ -33,7 +33,7 @@ function UploadProduct() {
     <div className="self-center flex flex-col   px-8">
       <div className="flex flex-col  max-md:max-w-full">
         <form onSubmit={productDataHandler}>
-        <div className="bg-white self-center flex w-full max-w-[1141px] flex-col  mt-7 pl-12 rounded-3xl max-md:max-w-full max-md:pl-5 border-2 border-blue-400">
+        <div className="bg-white self-center flex w-full max-w-[1141px] flex-col  mt-7 px-12 rounded-3xl max-md:max-w-full max-md:pl-5 border-2 border-blue-400">
           <div className="flex  w-full justify-between gap-5  pr-9  max-md:max-w-full max-md:flex-wrap max-md:pr-5 max-md:justify-center mt-11 border-2 border-red-400">
             <div className="text-stone-950 justify-start text-xl font-medium leading-8  whitespace-nowrap ">
               Adding New Products
@@ -57,10 +57,10 @@ function UploadProduct() {
               </button>
             </div>
           </div>
-          <div className="mt-0 max-md:max-w-full max-md:my-10 p-8 justify-center items-center border-2 border-yellow-400">
+          
             <div className="gap-5 flex max-lg:flex-col max-lg:justify-center max-md:items-stretch max-md:gap-5 border-2 border-green-400">
               <div className="flex flex-col items-stretch w-[28%] max-md:w-full max-md:ml-0 border-2 border-purple-400">
-                <div className="flex flex-col items-center max-md:mt-9 ">
+                <div className="flex flex-col items-center max-md:mt-9 border-2 border-green-400">
                   <label
                     htmlFor="dropzone-file"
                     className="bg-stone-300 flex w-[250px] max-w-full flex-col justify-center items-center px-16 py-12 max-md:px-5 cursor-pointer"
@@ -73,7 +73,7 @@ function UploadProduct() {
                     <input id="dropzone-file" type="file" className="hidden" />
                   </label>
 
-                  <div className=" w-[250px] max-w-full justify-center items-center self-stretch flex gap-3 mt-7 max-md:justify-center">
+                  <div className=" w-[250px] max-w-full justify-center items-center self-stretch flex gap-3 mt-7 max-md:justify-center border-2 border-green-400">
                     <img
                       loading="lazy"
                       src={LeftArrow}
@@ -119,7 +119,7 @@ function UploadProduct() {
                     value={productData.description}
                     onChange={(e) => setProductData({ ...productData, description: e.target.value })}
                   ></textarea>
-                  <div className="justify-between items-stretch self-stretch flex gap-5 mt-0 max-md:max-w-full max-md:flex-wrap border-2 border-orange-400">
+                  <div className="justify-between items-stretch self-stretch flex flex-col gap-5 mt-0 lg:flex-row max-md:max-w-full max-md:flex-wrap border-2 border-orange-400">
                     <div className="w-full items-stretch flex grow basis-[0%] flex-col mt-6">
                       <label className="text-stone-600 text-[14px] font-semibold leading-4 whitespace-nowrap">
                         Type
@@ -154,7 +154,7 @@ function UploadProduct() {
                     </div>
                   </div>
 
-                  <div className="justify-between items-stretch self-stretch flex gap-5 mt-0 max-md:max-w-full max-md:flex-wrap border-2 border-orange-400">
+                  <div className="justify-between items-stretch self-stretch flex flex-col gap-5 mt-0 lg:flex-row max-md:max-w-full max-md:flex-wrap border-2 border-orange-400">
                     <div className="w-full items-stretch flex grow basis-[0%] flex-col mt-6">
                       <label className="text-stone-600 text-[14px] font-semibold leading-4 whitespace-nowrap">
                       Inventory start at
@@ -186,7 +186,7 @@ function UploadProduct() {
                 </div>
               </div>
             </div>
-          </div>
+          
         </div>
         </form>
       </div>

bdeef1c6efe25d835f69b1dcbf313f6568fcec5c
jonathanj101 <jonatjimen@gmail.com>
2024-02-11 18:02:01 -0500

    handling feedback modal reducer,fixed filtering & pagination bug

--- src/pages/Sellers/OrderManagement/Returns-And-Refunds/ReturnsAndRefunds.jsx
+++ src/pages/Sellers/OrderManagement/Returns-And-Refunds/ReturnsAndRefunds.jsx
@@ -5,7 +5,6 @@ import PropTypes from "prop-types";
 
 // Components
 import Download from "./Components/Download/Download";
-import Feedback from "./Components/Feedback/Feedback";
 import FiltersComponent from "./Components/Filter/FiltersComponent";
 import Completed from "./Components/ReturnsOverview/Completed";
 import Initiated from "./Components/ReturnsOverview/Initiated";
@@ -13,28 +12,44 @@ import Received from "./Components/ReturnsOverview/Received";
 import SearchBar from "./Components/Search-bar/SearchBar";
 import Table from "./Components/Table/Table";
 
+// icons
+import arrowDown from "../../../../assets//ICONS/ArrowDown.svg";
+
 // redux
 import {
     searchDisputes,
     toggleFilterDisputes
 } from "../../../../reducersAndActions/actions/disputeAction";
 
+import { setDisplayFeedbackModal } from "../../../../reducersAndActions/actions/feedbackAction";
+import FeedbackModal from "../../../Buyers/FeedbackModal";
+
 const mapStateToProps = (state) => {
     return {
         disputes: state.disputeReducer.disputes,
-        filtered: state.disputeReducer.filtered
+        filtered: state.disputeReducer.filtered,
+        displayFeedbackModal: state.feedbackReducer.displayFeedbackModal
     };
 };
 
 const mapDispatchToProps = (dispatch) => ({
     searchDisputes: (data) => dispatch(searchDisputes(data)),
-    toggleFilterDisputes: (data) => dispatch(toggleFilterDisputes(data))
+    toggleFilterDisputes: (data) => dispatch(toggleFilterDisputes(data)),
+    setDisplayFeedbackModal: () => dispatch(setDisplayFeedbackModal(true))
 });
 
-function ReturnsAndRefunds({ disputes, searchDisputes, filtered, toggleFilterDisputes }) {
+function ReturnsAndRefunds({
+    disputes,
+    searchDisputes,
+    filtered,
+    toggleFilterDisputes,
+    setDisplayFeedbackModal,
+    displayFeedbackModal
+}) {
     const [disputesCopy, setDisputesCopy] = useState([]);
     const [itemIndex, setItemIndex] = useState(undefined); // selected item index, one single item
     const [isResetPagination, setResetPagination] = useState(false);
+    const [open, setOpen] = useState(true);
 
     useEffect(() => {
         disputes.forEach((item) => (item.selected = false));
@@ -54,6 +69,10 @@ function ReturnsAndRefunds({ disputes, searchDisputes, filtered, toggleFilterDis
 
     const resetPagination = () => {
         setResetPagination(!isResetPagination);
+    };
+
+    const handleFeedbackModal = () => {
+        setDisplayFeedbackModal();
     };
     return (
         <div className="self-center flex w-full max-w-full flex-col items-stretch p-5">
@@ -75,21 +94,46 @@ function ReturnsAndRefunds({ disputes, searchDisputes, filtered, toggleFilterDis
                     resetPagination={resetPagination}
                 />
             </div>
-            <div className="flex justify-center my-5">
-                <Initiated />
-                <Received />
-                <Completed />
+            <div className="flex my-5">
+                <div className="mr-5">
+                    <p>Insights</p>
+                </div>
+                <div>
+                    <button
+                        type="button"
+                        className="flex"
+                        onClick={() => setOpen(!open)}
+                    >
+                        Hide
+                        <img src={arrowDown} />
+                    </button>
+                </div>
             </div>
+            {open ? (
+                <div className="flex justify-center my-5">
+                    <Initiated />
+                    <Received />
+                    <Completed />
+                </div>
+            ) : (
+                <div></div>
+            )}
+
             <div>
-                <FiltersComponent
-                    disputes={disputes}
-                    disputesFiltered={filtered}
-                    disputesCopy={disputesCopy}
-                    resetFilter={resetFilter}
-                    updateCopy={updateCopy}
-                    resetPagination={resetPagination}
-                    toggleFilterDisputes={toggleFilterDisputes}
-                />
+                {open ? (
+                    <FiltersComponent
+                        disputes={disputes}
+                        filtered={filtered}
+                        disputesCopy={disputesCopy}
+                        resetFilter={resetFilter}
+                        updateCopy={updateCopy}
+                        resetPagination={resetPagination}
+                        toggleFilterDisputes={toggleFilterDisputes}
+                    />
+                ) : (
+                    <div></div>
+                )}
+
                 <Download
                     disputes={disputes}
                     itemIndex={itemIndex}
@@ -105,8 +149,17 @@ function ReturnsAndRefunds({ disputes, searchDisputes, filtered, toggleFilterDis
                     isResetPagination={isResetPagination}
                 />
             </div>
-            <div className="my-5">
-                <Feedback />
+            <div className="my-5 w-[75%]">
+                <div className="text-end">
+                    <button
+                        onClick={() => handleFeedbackModal()}
+                        className="border-2 border-gray-300 p-2 rounded-md "
+                        type="button"
+                    >
+                        Feedback
+                    </button>
+                    {displayFeedbackModal && <FeedbackModal />}
+                </div>
             </div>
         </div>
     );

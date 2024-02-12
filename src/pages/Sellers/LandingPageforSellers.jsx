7aa68e086e17a53074a84df22e301304f05a2ecb
Aaron Yii <aaronyii@mail.usf.edu>
2024-02-12 14:17:15 -0600

    Added Responsive features to signup page from seller side

--- src/pages/Sellers/LandingPageforSellers.jsx
+++ src/pages/Sellers/LandingPageforSellers.jsx
@@ -31,37 +31,37 @@ export default function LandingPageforSellers() {
   return (
     <div className="bg-zinc-100 relative min-h-max">
       {!isSignupPage && <Header onClick={showSideBar}/>}
-      <div className="w-full flex items-stretch mt-[72px] md:mt-[107px] min-h-screen">
+      <div className={`w-full flex items-stretch ${isSignupPage ? "" : "mt-[72px] md:mt-[107px]"} min-h-screen`}>
 
-      {!isSignupPage && <Sidebar showBarIsTrue={showBar}/>}
+        {!isSignupPage && <Sidebar showBarIsTrue={showBar}/>}
 
-      <div className={`${showBar ? "max-md:opacity-25" : "max-md:opacity-100"} flex flex-col items-stretch overflow-hidden grow`} onClick={showBar ? showSideBar : ()=>{}}>
+        <div className={`${showBar ? "max-md:opacity-25" : "max-md:opacity-100"} flex flex-col items-stretch overflow-hidden grow`} onClick={showBar ? showSideBar : ()=>{}}>
 
-        <Routes>
-          <Route path='/' element={<Homepage/>} />
-          <Route path='/signup' element={<Signup/>} />
-          <Route path='/login' element={<Login/>} />
+          <Routes>
+            <Route path='/' element={<Homepage/>} />
+            <Route path='/signup' element={<Signup/>} />
+            <Route path='/login' element={<Login/>} />
 
-          {/* Analytics Routing */}
-          <Route path='/overview' element={<Overview />} />
-          <Route path='/payments' element={<Payments />} />
-          <Route path='/activityfeed/*' element={<ActivityFeed />} />
-          <Route path='/activityfeed/FeedDetail/:id' element={<FeedDetail />} />
-          <Route path="/ratingsandreviews" element={<RatingsAndReviews />} />   
+            {/* Analytics Routing */}
+            <Route path='/overview' element={<Overview />} />
+            <Route path='/payments' element={<Payments />} />
+            <Route path='/activityfeed/*' element={<ActivityFeed />} />
+            <Route path='/activityfeed/FeedDetail/:id' element={<FeedDetail />} />
+            <Route path="/ratingsandreviews" element={<RatingsAndReviews />} />   
 
-          {/* Product Catalogue Routing */}
-          <Route path='/listOfproducts' element={<ListOfProducts />} />
-          <Route path='/uploadproduct' element={<UploadProduct />} />
-          <Route path='/product' element={<ProductDetail />} />
+            {/* Product Catalogue Routing */}
+            <Route path='/listOfproducts' element={<ListOfProducts />} />
+            <Route path='/uploadproduct' element={<UploadProduct />} />
+            <Route path='/product' element={<ProductDetail />} />
 
-          {/* Order Management Routing */}
-          <Route path='/orderlist' element={<OrderList />} />
-          <Route path='/returnsandrefunds' element={<ReturnsAndRefunds />} />
-          <Route path='/disputes' element={<Disputes />} />
+            {/* Order Management Routing */}
+            <Route path='/orderlist' element={<OrderList />} />
+            <Route path='/returnsandrefunds' element={<ReturnsAndRefunds />} />
+            <Route path='/disputes' element={<Disputes />} />
 
-          {/* Notification Setting */}
-          <Route path='/notificationsettings' element={<Body />} />
-        </Routes>
+            {/* Notification Setting */}
+            <Route path='/notificationsettings' element={<Body />} />
+          </Routes>
         </div>
       </div>
     </div>
--- src/pages/Sellers/Signup/Signup.jsx
+++ src/pages/Sellers/Signup/Signup.jsx
@@ -41,145 +41,145 @@ function Signup() {
   
 
   return (
-    <div className="bg-white">
-      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <div className="flex flex-col items-stretch w-6/12 max-md:w-full max-md:ml-0">
-          <div className="items-center flex grow flex-col justify-center px-16 py-12 max-md:max-w-full max-md:mt-10 max-md:px-5">
-            <div className="flex w-[484px] max-w-full flex-col items-stretch mt-16 mb-5 max-md:mt-10">
-              <span className="text-gray-900 text-3xl font-bold leading-10 max-md:max-w-full">
+      <div className="bg-white flex flex-col lg:flex-row max-md:items-stretch max-md:gap-0 grow">
+        {/* left part */}
+        <div className="flex flex-col gap-4 items-stretch w-full lg:w-[50%] p-8 sm:p-20 md:p-[60px] lg:p-[80px] xl:p-[120px]">
+          <div className='flex flex-col gap-4 items-center lg:items-start'>
+              <div className="text-gray-900 text-3xl font-bold leading-10 max-md:max-w-full text-center lg:text-left">
                 Sell your products easily starting from now!
-              </span>
-              <span className="text-gray-900 text-lg leading-7 mt-4 max-md:max-w-full">
+              </div>
+              <div className="text-gray-900 text-lg leading-7 mt-4 max-md:max-w-full">
                 Get started for free today!
-              </span>
-              <form onSubmit={handleSubmit}>
-                <div className="w-full items-stretch flex grow basis-[0%] flex-col mt-6">
-                  <label className="text-gray-900 text-sm font-medium leading-6 grow whitespace-nowrap">
-                    Name <span className="text-red-600"> * </span>
-                  </label>
-
-                  <input
-                    type="text"
-                    className="items-stretch border border-[color:var(--Primary-Base,#27A376)] flex flex-col justify-center mt-2.5 px-5 py-4 rounded-xl border-solid max-md:max-w-full"
-                    placeholder='Input your full name'
-                    value={storeData.name}
-                    onChange={(e) => [handleInputChange(e, 'name'), formErrors.name=""]}
-                    name="name"
-
-                  />
-                  {formErrors.name && <p className='text-red-600 mt-2'>{formErrors.name}</p>}
-                </div>
-
-                <div className="w-full items-stretch flex grow basis-[0%] flex-col mt-6">
-                  <label className="text-gray-900 text-sm font-medium leading-6 grow whitespace-nowrap">
-                    Work Email <span className="text-red-600"> * </span>
-                  </label>
-
-                  <input
-                    type="email"
-                    className="items-stretch border border-[color:var(--Primary-Base,#27A376)] flex flex-col justify-center mt-2.5 px-5 py-4 rounded-xl border-solid max-md:max-w-full"
-                    placeholder='example@company.com'
-                    value={storeData.workEmail}
-                    onChange={(e) => [handleInputChange(e, 'workEmail'), formErrors.workEmail=""]}
-                    name="workEmail"
-
-                  />
-                  {formErrors.workEmail && <p className='text-red-600 mt-2'>{formErrors.workEmail}</p>}
-                </div>
-                <div className="w-full items-stretch flex grow basis-[0%] flex-col mt-6">
-                  <label className="text-gray-900 text-sm font-medium leading-6 grow whitespace-nowrap">
-                    Legal business name <span className="text-red-600"> * </span>
-                  </label>
-
-                  <input
-                    type="text"
-                    className="items-stretch border border-[color:var(--Primary-Base,#27A376)] flex flex-col justify-center mt-2.5 px-5 py-4 rounded-xl border-solid max-md:max-w-full"
-                    placeholder='Shop online new york'
-                    value={storeData.legalBusinessName}
-                    onChange={(e) => [handleInputChange(e, 'legalBusinessName'), formErrors.legalBusinessName=""]}
-                    name="legalBusinessName"
-
-                  />
-                  {formErrors.legalBusinessName && <p className='text-red-600 mt-2'>{formErrors.legalBusinessName}</p>}
-                </div>
-                <div className="w-full items-stretch flex grow basis-[0%] flex-col mt-6">
-                  <label className="text-gray-900 text-sm font-medium leading-6 grow whitespace-nowrap">
-                    Password <span className="text-red-600"> * </span>
-                  </label>
-
-                  <input
-                    type="password"
-                    className="items-stretch border border-[color:var(--Primary-Base,#27A376)] flex flex-col justify-center mt-2.5 px-5 py-4 rounded-xl border-solid max-md:max-w-full"
-                    placeholder='Input your password account'
-                    value={storeData.password}
-                    onChange={(e) => [handleInputChange(e, 'password'), formErrors.password=""]}
-                    name="password"
-                  />
-                  {formErrors.password && <p className='text-red-600 mt-2'>{formErrors.password}</p>}
-
-                </div>
-
-                <button
-                  type='submit'
-                  className="w-full text-[#A0AEC0] text-center text-base font-bold leading-6 tracking-wide whitespace-nowrap justify-center items-center bg-[#F1F2F4] mt-5 px-16 py-4 rounded-xl max-md:max-w-full max-md:px-5">
-                  Create Account
-                </button>
-              </form>
-
-              <div className=" w-full justify-center items-stretch flex gap-4 mt-4 px-0.5 py-2.5 max-md:max-w-full max-md:flex-wrap">
-                <div className="items-center bg-gray-100 self-center flex w-[25%] shrink-0 h-px flex-col my-auto" />
-                <span className="text-slate-500 text-center text-sm font-medium leading-6">
-                  Or register with
-                </span>
-                <div className="items-center bg-gray-100 self-center flex w-[25%] shrink-0 h-px flex-col my-auto" />
               </div>
-              <div className="items-stretch flex justify-between gap-4 mt-4 max-md:max-w-full max-md:flex-wrap">
-                <button
-                  type='button'
-                  className="justify-between border border-[color:var(--Greyscale-300,#E9EAEC)] flex gap-3 px-20 py-4 rounded-xl border-solid items-start max-md:px-5">
-                  <img
-                    loading="lazy"
-                    src={google}
-                    className="aspect-square object-contain object-center w-[22px] overflow-hidden shrink-0 max-w-full"
-                  />
-                  <div className="text-gray-900 text-center text-base font-medium leading-6 tracking-wide self-stretch">
-                    Google
-                  </div>
-                </button>
+          </div>
+          <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+            <div className="items-stretch flex flex-col gap-[10px]">
+              <label className="text-gray-900 text-sm font-medium leading-6 grow whitespace-nowrap">
+                Name <span className="text-red-600"> * </span>
+              </label>
 
-                <button
-                  type='button'
-                  className="justify-between border border-[color:var(--Greyscale-300,#E9EAEC)] flex gap-4 px-20 py-4 rounded-xl border-solid items-start max-md:px-5">
-                  <img
-                    loading="lazy"
-                    src={apple}
-                    className="aspect-[0.91] object-contain object-center w-5 fill-gray-900 overflow-hidden shrink-0 max-w-full"
-                  />
-                  <div className="text-gray-900 text-center text-base font-medium leading-6 tracking-wide self-stretch">
-                    Apple
-                  </div>
-                </button>
+              <input
+                type="text"
+                className="border focus:border-[color:var(--Primary-Base,#27A376)] border-[#A0AEC0] outline-none px-5 py-4 rounded-xl border-solid"
+                placeholder='Input your full name'
+                value={storeData.name}
+                onChange={(e) => [handleInputChange(e, 'name'), formErrors.name=""]}
+                name="name"
+
+              />
+              {formErrors.name && <p className='text-red-600 mt-2'>{formErrors.name}</p>}
+            </div>
+
+            <div className="items-stretch flex flex-col gap-[10px]">
+              <label className="text-gray-900 text-sm font-medium leading-6 grow whitespace-nowrap">
+                Work Email <span className="text-red-600"> * </span>
+              </label>
+
+              <input
+                type="email"
+                className="border focus:border-[color:var(--Primary-Base,#27A376)] border-[#A0AEC0] outline-none px-5 py-4 rounded-xl border-solid"
+                placeholder='example@company.com'
+                value={storeData.workEmail}
+                onChange={(e) => [handleInputChange(e, 'workEmail'), formErrors.workEmail=""]}
+                name="workEmail"
+
+              />
+              {formErrors.workEmail && <p className='text-red-600 mt-2'>{formErrors.workEmail}</p>}
+            </div>
+            <div className="items-stretch flex flex-col gap-[10px]">
+              <label className="text-gray-900 text-sm font-medium leading-6 grow whitespace-nowrap">
+                Legal business name <span className="text-red-600"> * </span>
+              </label>
+
+              <input
+                type="text"
+                className="border focus:border-[color:var(--Primary-Base,#27A376)] border-[#A0AEC0] outline-none px-5 py-4 rounded-xl border-solid"
+                placeholder='Shop online new york'
+                value={storeData.legalBusinessName}
+                onChange={(e) => [handleInputChange(e, 'legalBusinessName'), formErrors.legalBusinessName=""]}
+                name="legalBusinessName"
+
+              />
+              {formErrors.legalBusinessName && <p className='text-red-600 mt-2'>{formErrors.legalBusinessName}</p>}
+            </div>
+            <div className="items-stretch flex flex-col gap-[10px]">
+              <label className="text-gray-900 text-sm font-medium leading-6 grow whitespace-nowrap">
+                Password <span className="text-red-600"> * </span>
+              </label>
+
+              <input
+                type="password"
+                className="border focus:border-[color:var(--Primary-Base,#27A376)] border-[#A0AEC0] outline-none px-5 py-4 rounded-xl border-solid"
+                placeholder='Input your password account'
+                value={storeData.password}
+                onChange={(e) => [handleInputChange(e, 'password'), formErrors.password=""]}
+                name="password"
+              />
+              {formErrors.password && <p className='text-red-600 mt-2'>{formErrors.password}</p>}
+
+            </div>
+
+            <button
+              type='submit'
+              className="ease-in-out duration-200 text-[#A0AEC0] text-center text-base font-bold leading-6 tracking-wide whitespace-nowrap justify-center items-center hover:bg-[#1e1564] hover:text-[#F1F2F4] bg-[#F1F2F4] mt-5 px-16 py-4 rounded-xl max-md:max-w-full">
+              Create Account
+            </button>
+          </form>
+          <div className="justify-center items-center flex px-0.5 py-2.5 max-md:max-w-full max-md:flex-wrap">
+            <div className="bg-gray-100 grow h-px" />
+            <span className=" text-slate-500 text-center text-sm font-medium leading-6 px-10 grow-0">
+              Or register with
+            </span>
+            <div className="bg-gray-100 grow h-px" />
+          </div>
+          <div className="items-stretch flex flex-col sm:flex-row justify-between gap-4">
+            <button
+              type='button'
+              className="grow justify-center border border-[color:var(--Greyscale-300,#E9EAEC)] flex gap-3 px-20 lg:px-2 xl:px-4 py-4 rounded-xl border-solid items-center">
+              <img
+                loading="lazy"
+                src={google}
+                className="aspect-square object-contain object-center w-[22px] overflow-hidden shrink-0 max-w-full"
+              />
+              <div className="text-gray-900 text-center text-base font-medium leading-6 tracking-wide self-stretch">
+                Google
               </div>
-              <div className="text-emerald-500 text-sm font-medium leading-6 mt-4 max-md:max-w-full">
-                <span className="text-slate-400">
-                  Already have an account? <Link to="/sellers/login">Login</Link>
-                </span>
+            </button>
+
+            <button
+              type='button'
+              className="grow justify-center border border-[color:var(--Greyscale-300,#E9EAEC)] flex gap-3 px-20 lg:px-2 xl:px-4 py-4 rounded-xl border-solid items-center">
+              <img
+                loading="lazy"
+                src={apple}
+                className="aspect-square object-contain object-center w-[22px] overflow-hidden shrink-0 max-w-full"
+              />
+              <div className="px-[5px] text-gray-900 text-center text-base font-medium leading-6 tracking-wide self-stretch">
+                Apple 
               </div>
-              <span className="justify-center items-stretch flex gap-2.5 mt-2.5 max-md:max-w-full max-md:flex-wrap">
-                <div className="text-slate-400 text-sm font-medium leading-6 grow whitespace-nowrap">
-                  © 2023 Humanline. All Rights Reserved.
-                </div>
-                <div className="text-gray-900 text-sm font-medium leading-6">
-                  Terms & Conditions
-                </div>
-                <div className="text-gray-900 text-sm font-medium leading-6 whitespace-nowrap">
-                  Privacy Policy
-                </div>
-              </span>
+            </button>
+          </div>
+          <div className="text-sm font-medium leading-6 text-center">
+            <span className="text-slate-400">
+              Already have an account? <Link to="/sellers/login" className='text-[#27A376]'>Login</Link>
+            </span>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <div className="text-slate-400 text-sm font-medium leading-6 grow whitespace-nowrap">
+              © 2023 Humanline. All Rights Reserved.
+            </div>
+            <div className='flex justify-center items-center gap-5'>
+              <div className="text-gray-900 text-[11px] font-medium leading-6">
+                Terms & Conditions
+              </div>
+              <div className="text-gray-900 text-[11px] font-medium leading-6 whitespace-nowrap">
+                Privacy Policy
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-stretch w-6/12 ml-5 max-md:w-full max-md:ml-0">
+        {/* right part */}
+        <div className="hidden lg:flex flex-col items-stretch w-[50%]">
           <div className="bg-amber-300 grow w-full max-md:max-w-full max-md:mt-10">
             <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
               <div className="flex flex-col items-stretch w-full max-md:w-full max-md:ml-0">
@@ -204,7 +204,6 @@ function Signup() {
           </div>
         </div>
       </div>
-    </div>
   )
 }
 

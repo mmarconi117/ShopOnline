00151c801a61294540662c0ef5f37b8a8948bcd1
Nathan <nathan.forman@gmail.com>
2024-02-15 15:36:40 -0800

    Finishing button layout for R and R page

--- src/pages/Sellers/Analytics/R&R/Ratings.jsx
+++ src/pages/Sellers/Analytics/R&R/Ratings.jsx
@@ -13,6 +13,7 @@ import {
 import picDemo from './assets/picDemo.jpeg';
 import picDemo2 from './assets/picDemo2.jpeg';
 import picDemo3 from './assets/picDemo3.webp';
+import Union_Stroke from './assets/Union_Stroke.jpg';
 
 const RatingsAndReviews = ({
     reviews,
@@ -159,7 +160,7 @@ const RatingsAndReviews = ({
     };
 
     return (
-        <div className="m-5 border-2 border-red-400">
+        <div className="m-5">
             <div className="ratings-and-reviews">
                 <h1 style={{ fontWeight: 'bold' }}>Ratings and Reviews</h1>
                 <p>Customers provide Seller ratings and reviews. These ratings and reviews indicate whether you are creating a positive shopping experience for customers.</p>
@@ -169,7 +170,7 @@ const RatingsAndReviews = ({
                 <div className="h-8 w-1 bg-slate-300"></div>
                 <p>Average Rating: {reviewStats.averageRating.toFixed(2)}</p>
             </div>
-            <div className="sort-by-dropdown flex justify-between border-2 border-green-400">
+            <div className="sort-by-dropdown flex justify-between">
                 <div className="">
                     <label className="mr-5">Sort by:</label>
                     <select onChange={(e) => handleSortByChange(e.target.value)}>
@@ -181,7 +182,7 @@ const RatingsAndReviews = ({
                 <div>{getCurrentMonthAndYear()}</div>
             </div>
 
-            <div className="hidden review-form flex m-5 bg-white border-2 border-blue-400">
+            <div className="hidden review-form flex m-5 bg-white">
                 <form onSubmit={handleSubmit}>
                     <div style={{ marginTop: '20px' }}>
                         <label>Name: </label>
@@ -205,26 +206,34 @@ const RatingsAndReviews = ({
                 {/* {reviews.map((review) => (
                     <ReviewItem key={review.id} review={review} />
                 ))} */}
-                <div className="review-item h-100 flex justify-between m-5 p-5 bg-white border-2 border-blue-400">
-                    <div className="w-full flex border-2 border-orange-400">
+                <div className="review-item h-100 flex justify-between m-5 p-5 bg-white">
+                    <div className="w-full flex">
                         <img className="mr-3" src={picDemo} alt="Random" style={{ width: '100px', height: '100px' }} />
                         <div className="flex flex-col">
                             <h3>Random Name</h3>
                             <p>{convertToStars(4)}</p>
                         </div>
                     </div>
-                    <div className="w-full flex flex-wrap border-2 text-ellipsis border-orange-400">
+                    <div className="w-full flex flex-wrap text-ellipsis">
                         <p>I’m super happy with these!  I’ve never bought dress online before and I didn’t think they’d even fit, but it turns out they fit pretty perfectly. I got a size S Patite - I’m 5’6” and weigh about 127 lbs. They are tight but not suffocating ...I’m super happy with these!  I’ve never bought dress online before and I didn’t think they’d even fit, but it turns out they fit pretty perfectly. I got a size S Patite - I’m 5’6” and weigh about 127 lbs. They are tight but not suffocating ...</p>
-                        <div style={{ marginTop: '10px' }}>
-                            <button  style={{ marginRight: '10px' }}>
+                        <div className="flex justify-center" style={{ marginTop: '10px' }}>
+                            <button className="text-gray-800 bg-white border-2 border-yellow-400 font-medium rounded-lg text-sm px-5 py-3" style={{ marginRight: '10px' }}>
                                 {false ? 'Cancel' : 'Public Comment'}
                             </button>
-                            <button  style={{ marginRight: '10px' }}>
+                            <button className="text-gray-800 bg-yellow-400 font-medium rounded-lg text-sm px-5 py-3" style={{ marginRight: '10px' }}>
                                 {false ? 'Cancel' : 'Direct Message'}
                             </button>
-                            <button style={{ marginRight: '10px' }} onClick={() => toggleLike(1)}>
-                                {false ? 'Dislike' : 'Like'}
+                            <button className="w-12 h-12 p-3 border-2 border-yellow-400 rounded-lg" style={{ marginRight: '10px' }} onClick={() => toggleLike(1)}>
+                                <svg className="flex justify-center fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M6.45 2C4.257 2 2.5 3.753 2.5 5.889c0 1.04.414 1.985 1.093 2.686l6.907 7L17.28 8.7A3.845 3.845 0 0 0 18.5 5.89C18.5 3.753 16.743 2 14.55 2c-.476 0-1.034.267-1.654.816-.604.536-1.146 1.23-1.582 1.842a1 1 0 0 1-1.628 0c-.436-.612-.978-1.306-1.582-1.842C7.484 2.266 6.926 2 6.45 2ZM.5 5.889C.5 2.625 3.176 0 6.45 0c1.198 0 2.218.644 2.98 1.319.392.348.752.735 1.07 1.116.319-.381.678-.768 1.07-1.116C12.332.644 13.352 0 14.55 0c3.274 0 5.95 2.625 5.95 5.889a5.845 5.845 0 0 1-1.805 4.224l-7.483 7.59a1 1 0 0 1-1.424 0L2.162 9.97A5.841 5.841 0 0 1 .5 5.89Z" clip-rule="evenodd"/>
+                                </svg>
                             </button>
+                            {/* {false ? 
+                                'Dislike' 
+                                : 
+                                
+                            } */}
+                            
                         </div>
                         {false && (
                             <div style={{ marginTop: '10px' }}>

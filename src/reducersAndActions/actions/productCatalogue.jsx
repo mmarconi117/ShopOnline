4e48032f18a759f87452f14ffa5f629b4ec40a56
jonathanj101 <jonatjimen@gmail.com>
2024-02-16 13:29:16 -0500

    implemented actions/reducers for filters functionalities

--- src/reducersAndActions/actions/productCatalogue.jsx
+++ src/reducersAndActions/actions/productCatalogue.jsx
@@ -3,3 +3,13 @@ import * as actionTypes from "../actions";
 export const productCatalogue = () => ({
     type: actionTypes.PRODUCT_CATALOGUE,
 });
+
+export const searchProduct = (data) => ({
+    type: actionTypes.SEARCH_PRODUCT,
+    payload: data
+});
+
+export const sortCatalogue = (data) => ({
+    type: actionTypes.SORT_CATALOGUE,
+    payload: data
+});

4d06ba8b26eff1c9e3401e4cc8a49644d7680ab6
Nathan <nathan.forman@gmail.com>
2024-03-07 14:56:26 -0800

    Working on Checkout page on Buyers side

--- src/pages/Buyers/Checkout/Checkout.jsx
+++ src/pages/Buyers/Checkout/Checkout.jsx
@@ -12,20 +12,32 @@ const Checkout = () => {
   };
 
   return (
-    <div className="bg-[#F0F0F0]">
-      <div className="flex px-4 pt-8 flex-col mx-auto gap-8 lg:grid grid-cols-3 lg:max-h-[827px] lg:px-10 lg:pt-[68px]">
-        <div className="flex flex-col justify-center items-start col-start-1 col-end-3 bg-blue-200 lg:h-[759px]">
+    <div className="bg-[#F0F0F0] border-2 border-red-400">
+      <div className="flex px-4 pt-8 flex-col mx-auto gap-8 lg:grid grid-cols-3 lg:max-h-[827px] lg:px-10 lg:pt-[68px] border-2 border-blue-400">
+        <div className="flex flex-col justify-center items-start col-start-1 col-end-3 bg-blue-200 lg:h-[759px] border-2 border-green-400">
+          <div className="flex w-full h-150 bg-white font-bold">
+            Shipment Address
+            {/* <div className="w-full grey leading-[3rem]"></div> */}
+          </div>
+          <div className="flex w-full h-150 bg-white font-bold">
+            Payment Method
+            {/* <div className="w-full grey leading-[3rem]"></div> */}
+          </div>
+          <div className="flex w-full h-150 bg-white font-bold">
+            Shipping Method
+            {/* <div className="w-full grey leading-[3rem]"></div> */}
+          </div>
           {carts.map((item, index) => (
             <div key={index} className="border border-gray-300 mb-4 p-4">
               <img
                 src={item.product.avatar}
                 className="w-24 h-24 mb-2"
               />
-              <div className="info">
+              <div className="info border-2 border-orange-400">
                 <h3 className="text-lg font-semibold mb-1">{item.product.name}</h3>
                 <p className="text-sm mb-1">{item.product.description}</p>
                 <p className="text-sm mb-1">${item.product.price}</p>
-                <div className="flex items-center">
+                <div className="flex items-center border-2 border-yellow-400">
                   <span className="text-gray-700 mr-2">Quantity:</span>
                   <span>{item.quantity}</span>
                 </div>
@@ -58,7 +70,14 @@ const Checkout = () => {
             >
               Checkout
             </button>
+            
           </div>
+          <div className="flex flex-col w-full h-150 p-5 bg-white">
+              <div className="text-xl">Returns are easy</div>
+              <div>Free return within 15 days for Official Store items
+              and 7 days for other eligible items. <a href="">See more</a> 
+              </div>
+            </div>
         </div>
 
 

dd3c696be78f887c3212c437d1c80f1c207e2ee9
qizongliang <72285030+qizongliang@users.noreply.github.com>
2024-03-10 19:14:56 -0400

    UpperBody Page views

--- src/pages/Buyers/Category/JewelleryAndWatches/JewelleryAndWatchesSubCategory.jsx
+++ src/pages/Buyers/Category/JewelleryAndWatches/JewelleryAndWatchesSubCategory.jsx
@@ -1,5 +1,10 @@
+import UpperBody from './UpperBody/UpperBody.jsx'
 function JewelleryAndWatchesSubCategory() {
-  return <div className="bg-[#f5f5f9]"></div>
+  return (
+    <div className="bg-[#f5f5f9]">
+      <UpperBody></UpperBody>
+    </div>
+  )
 }
 
 export default JewelleryAndWatchesSubCategory

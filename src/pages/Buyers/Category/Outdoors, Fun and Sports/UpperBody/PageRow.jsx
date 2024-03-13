97918756f39c180ec925af4603aa2d8fcfc30c42
Wesley Roulaine <wtr1022@students.pensacolastate.edu>
2024-03-12 19:49:15 -0500

    Update PageRow

--- src/pages/Buyers/Category/Outdoors, Fun and Sports/UpperBody/PageRow.jsx
+++ src/pages/Buyers/Category/Outdoors, Fun and Sports/UpperBody/PageRow.jsx
@@ -13,7 +13,7 @@ function Pagerow() {
     <div
       className="bg-[#FFFFFF] w-full h-auto sm:h-[99px] sm:mt-[62px] flex flex-col sm:flex-row items-center font-normal text-[25px] text-[#48464C] leading-[30px] sm:gap-[102px] sm:overflow-x-auto p-2 sm:px-[54px] sm:py-4 max-sm:ml-[5px]"
     >
-      
+      <OutdoorsFunAndSportsView></OutdoorsFunAndSportsView>
       <div className="max-sm:w-full flex justify-between items-center gap-[22px] sm:gap-[102px] text-[#48464C] font-Roboto text-base font-medium sm:text-[25px] sm:font-normal sm:leading-[30px] max-sm:overflow-x-auto">
         <CyclingView></CyclingView>
         <CampingSuppliesView></CampingSuppliesView>

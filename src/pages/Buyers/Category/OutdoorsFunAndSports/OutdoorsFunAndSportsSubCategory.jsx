ded059d4f67ae7589b8c74b0f868da8666d96144
webdev-michael <webdev.krygore@gmail.com>
2024-03-19 11:55:20 -0700

    Fixed routing issues

--- src/pages/Buyers/Category/OutdoorsFunAndSports/OutdoorsFunAndSportsSubCategory.jsx
+++ src/pages/Buyers/Category/OutdoorsFunAndSports/OutdoorsFunAndSportsSubCategory.jsx
@@ -1,8 +1,8 @@
 import BikesRow from './Cycling/Bikes.jsx';
 import UpperBody from './UpperBody/UpperBody.jsx'
 import ClimbingRow from './ClimbingAndMountaineering/Climbing.jsx';
-import OutdoorRecreationRow from './OutdoorRecreation/OutdoorRec.jsx';
-import OutdoorAndSportsRow from './OutdoorsSports/Outdoor.jsx';
+import OutdoorRecreationRow from './OutdoorsRecreation/OutdoorRec.jsx';
+import OutdoorAndSportsRow from './OutdoorSports/Outdoor.jsx';
 import SurvivalAndNavigationRow from './SurvivalAndNavigation/Survival.jsx';
 import GymToolsRow from './GymTools/GymTools.jsx';
 

55a93b0655be7ed21629f0844820a71cfb68d492
jonathanj101 <jonatjimen@gmail.com>
2024-03-20 21:36:31 -0400

    deleted dropdown, added account endpoint

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -20,86 +20,102 @@ export default function Nav() {
   };
 
   return (
-        <ul className="flex bg-[#001c43] text-[#b7b9ba] justify-between gap-3 sm:justify-end sm:gap-10  py-2 px-3 ">
+      <ul className="flex bg-[#001c43] text-[#b7b9ba] justify-between gap-3 sm:justify-end sm:gap-10  py-2 px-3 ">
           <li
-            onClick={() => {
-              setMenu("home");
-            }}
+              onClick={() => {
+                  setMenu('home');
+              }}
           >
-            <Link to="/" style={{ textDecoration: "none" }}>
-              Home
-            </Link>
-            {menu === "home" ? (
-              <hr className="border-none w-full h-1 rounded-lg bg-[#09618E]" />
-            ) : (
-              <></>
-            )}
+              <Link to="/" style={{ textDecoration: 'none' }}>
+                  Home
+              </Link>
+              {menu === 'home' ? (
+                  <hr className="border-none w-full h-1 rounded-lg bg-[#09618E]" />
+              ) : (
+                  <></>
+              )}
           </li>
           <li
-            onClick={() => {
-              setMenu("sell");
-            }}
+              onClick={() => {
+                  setMenu('sell');
+              }}
           >
-            <Link 
-              to="/sellers/login" 
-              style={{ textDecoration: "none", color: isAuthenticated ? 'gray' : '' }}
-              className={`cursor-pointer ${isAuthenticated ? 'pointer-events-none opacity-50 cursor-not-allowed' : 'opacity-100'}`}
-            >
-              Sell
-            </Link>
-            {menu === "sell" ? (
-              <hr className="border-none w-full h-1 rounded-lg bg-[#09618E]" />
-            ) : (
-              <></>
-            )}
+              <Link
+                  to="/sellers/login"
+                  style={{
+                      textDecoration: 'none',
+                      color: isAuthenticated ? 'gray' : '',
+                  }}
+                  className={`cursor-pointer ${
+                      isAuthenticated
+                          ? 'pointer-events-none opacity-50 cursor-not-allowed'
+                          : 'opacity-100'
+                  }`}
+              >
+                  Sell
+              </Link>
+              {menu === 'sell' ? (
+                  <hr className="border-none w-full h-1 rounded-lg bg-[#09618E]" />
+              ) : (
+                  <></>
+              )}
           </li>
           <li
-            onClick={() => {
-              setMenu("language");
-            }}
+              onClick={() => {
+                  setMenu('language');
+              }}
           >
-            <Link to="/language" style={{ textDecoration: "none" }}>
-              English/US
-            </Link>
-            {menu === "language" ? (
-              <hr className="border-none w-full h-1 rounded-lg bg-[#09618E]" />
-            ) : (
-              <></>
-            )}
+              <Link to="/language" style={{ textDecoration: 'none' }}>
+                  English/US
+              </Link>
+              {menu === 'language' ? (
+                  <hr className="border-none w-full h-1 rounded-lg bg-[#09618E]" />
+              ) : (
+                  <></>
+              )}
           </li>
           <li
-            onClick={() => {
-              setMenu("help");
-            }}
+              onClick={() => {
+                  setMenu('help');
+              }}
           >
-            <Link to="/help" style={{ textDecoration: "none" }}>
-              Help
-            </Link>
-            {menu === "help" ? (
-              <hr className="border-none w-full h-1 rounded-lg bg-[#09618E]" />
-            ) : (
-              <></>
-            )}
+              <Link to="/help" style={{ textDecoration: 'none' }}>
+                  Help
+              </Link>
+              {menu === 'help' ? (
+                  <hr className="border-none w-full h-1 rounded-lg bg-[#09618E]" />
+              ) : (
+                  <></>
+              )}
           </li>
           <li>
-            <div style={{ position: "relative", display: "flex" }}>
-              <button className="dropbtn">
-                {isAuthenticated ? "Account" : ""}
-              </button>
-              <div className="flex gap-4 dropdown-content">
-                {isAuthenticated && (
-                  <>
-                    <Link to="/settings">Settings</Link>
-                    <Link to="/info">Info</Link>
-                    <Link to="/orders">Orders</Link>
-                    <button onClick={handleLogout}>Logout</button>
-                    {/* Add other menu items as needed */}
-                  </>
-                )}
-                {!isAuthenticated && <Link to="/login">Login</Link>}
+              <div style={{ position: 'relative', display: 'flex' }}>
+                  <button className="dropbtn">
+                      {isAuthenticated ? (
+                          <Link to="/account">Account</Link>
+                      ) : (
+                          ''
+                      )}
+                  </button>
+                  {/* no dropdwn */}
+                  <div className="flex gap-4 dropdown-content">
+                      {isAuthenticated && (
+                          <>
+                              <button onClick={handleLogout}>Logout</button>
+                              {/* Add other menu items as needed */}
+                          </>
+                      )}
+                      {!isAuthenticated && <Link to="/login">Login</Link>}
+                  </div>
               </div>
-            </div>
           </li>
-        </ul>
+      </ul>
   );
 }
+
+{/* <Link to="/settings">Settings</Link>
+<Link to="/info">Info</Link>
+<Link to="/orders">Orders</Link> */}
+
+// orders page not done
+// info page not done

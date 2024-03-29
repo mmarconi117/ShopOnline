9a59adfa981b444d6357efee54026878325a0863
Wesley Roulaine <wtr1022@students.pensacolastate.edu>
2024-03-29 18:59:47 -0500

    Added paths for the buttons.

--- src/components/Explore/BaseImagesAndCategories.jsx
+++ src/components/Explore/BaseImagesAndCategories.jsx
@@ -1,68 +1,101 @@
-/*
-This component created to house the base images and categories of the explore.jsx page.
-*/
+// BaseImagesAndCategories.jsx
 
 import beauty from "./assets/beauty.png";
 import bagsAndShoes from './assets/bagsAndShoes.png';
 import accessories from './assets/accessories.png';
 import electronics from './assets/electronics.png';
-import fashion from './assets/fashion.png'
+import fashion from './assets/fashion.png';
 import furniture from './assets/furniture.png';
 import groceries from './assets/groceries.png';
 import digital_media from "./assets/digital-media.png";
 import skinCare from './assets/skinCare.png';
-import wristWatches from './assets/wristWatches.png'
-import mensSneakers from './assets/mens_sneakers.png' // Added correct photo
-import health from './assets/health.png'
+import wristWatches from './assets/wristWatches.png';
+import mensSneakers from './assets/mens_sneakers.png'; // Added correct photo
+import health from './assets/health.png';
 
 export const BaseImagesAndCategories = [
-  {
-    category: 'Beauty',
-    img: beauty,
-  },
-  {
-    category: 'Fashion',
-    img: fashion,
-  },
-  {
-    category: 'Accessories',
-    img: accessories,
-  },
-  {
-    category: 'Furniture',
-    img: furniture,
-  },
-  {
-    category: 'Watches',
-    img: wristWatches,
-  },
-  {
-    category: `Men's Shoes`,
-    img: mensSneakers,
-  },
-  {
-    category: 'Grocery',
-    img: groceries,
-  },
-  {
-    category: 'Health',
-    img: health,
-  },
-  {
-    category: 'Tech',
-    img: electronics,
-  },
-  {
-    category: 'Skincare',
-    img: skinCare,
-  },
-  {
-    category: `Women's Shoes`,
-    img: bagsAndShoes,
-  },
-  {
-    category: 'Digital Media',
-    img: digital_media,
-  },
-  
-]
+    {
+        id: 'beauty',
+        path: '/beauty',
+        category: "Beauty",
+        img: beauty,
+        price: '120',
+    },
+    {
+        id: 'bagsAndShoes',
+        path: '/bagsAndShoes',
+        category: "Bags and Shoes",
+        img: bagsAndShoes,
+        price: '120',
+    },
+    {
+        id: 'accessories',
+        path: '/accessories',
+        category: "Accessories",
+        img: accessories,
+        price: '120',
+    },
+    {
+        id: 'electronics',
+        path: '/electronics',
+        category: "Electronics",
+        img: electronics,
+        price: '120',
+    },
+    {
+        id: 'fashion',
+        path: '/fashion',
+        category: "Fashion",
+        img: fashion,
+        price: '120',
+    },
+    {
+        id: 'furniture',
+        path: '/furniture',
+        category: "Furniture",
+        img: furniture,
+        price: '120',
+    },
+    {
+        id: 'groceries',
+        path: '/groceries',
+        category: "Groceries",
+        img: groceries,
+        price: '120',
+    },
+    {
+        id: 'digital_media',
+        path: '/digital_media',
+        category: "Digital Media",
+        img: digital_media,
+        price: '120',
+    },
+    {
+        id: 'skinCare',
+        path: '/skinCare',
+        category: "Skin Care",
+        img: skinCare,
+        price: '120',
+    },
+    {
+        id: 'wristWatches',
+        path: '/wristWatches',
+        category: "Wrist Watches",
+        img: wristWatches,
+        price: '120',
+    },
+    {
+        id: 'mensSneakers',
+        path: '/mensSneakers',
+        category: "Men's Sneakers",
+        img: mensSneakers,
+        price: '120',
+    },
+    {
+        id: 'health',
+        path: '/health',
+        category: "Health",
+        img: health,
+        price: '120',
+    },
+];

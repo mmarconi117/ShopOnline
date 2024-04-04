21e97dc9d5a71484491b9077fb9dde136be0d833
jonathanj101 <jonatjimen@gmail.com>
2024-04-04 13:17:15 -0400

    added links to their respective pages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,26 +7,56 @@ import SubscribeForm from "./SubscribeForm";
 // import globe from "../../assets/ICONS/mobileicons/globe.svg";
 
 const footerSectionsData = [
-  {
-    title: "LET US HELP YOU",
-    items: [
-      "Help Center",
-      "Contact Us",
-      "FAQs",
-      "Returns",
-      "Policies",
-    ],
-  },
-  {
-    title: "ABOUT YORK SHOPPERS",
-    items: [
-      "About Us",
-      "York Shoppers’ Express",
-      "Cookie Notice",
-      "Legal and Privacy Notices",
-      "Flash Sales",
-    ],
-  },
+    {
+        title: 'LET US HELP YOU',
+        items: [
+            {
+                title: 'Help Center',
+                link: '/help-center',
+            },
+            {
+                title: 'Contact Us',
+                link: '/contact-us',
+            },
+            {
+                title: 'FAQs',
+                link: '/faqs',
+            },
+            {
+                title: 'Returns',
+                link: '/returns-help',
+            },
+            {
+                title: 'Policies',
+                link: '/policies',
+            },
+        ],
+    },
+    {
+        title: 'ABOUT YORK SHOPPERS',
+        items: [
+            {
+                title: 'About Us',
+                link: '/about',
+            },
+            {
+                title: "York Shoppers' Express",
+                link: '/ys-express',
+            },
+            {
+                title: 'Cookie Notice',
+                link: '/cookie-notice',
+            },
+            {
+                title: 'Legal and Privacy Notices',
+                link: '/privacy-notices',
+            },
+            {
+                title: 'Flash Sales',
+                link: '/flash-sales',
+            },
+        ],
+    },
 ];
 
 const footerSectionsDataMobile = [
@@ -51,61 +81,65 @@ const footerSectionsDataMobile = [
 
 const Footer = () => {
   return (
-    <>
-      {/* Desktop Version */}
-      <div className="hidden px-6 lg:px-20 pt-16 flex-col border-[5px] bg-[#001c43] sm:flex gap-2">
-        <div className="flex justify-between items-start gap-6 lg:gap-10">
-          {footerSectionsData.map((section, index) => (
-            <InfoSection key={index} title={section.title}>
-              {section.items.map((item, i) => (
-                <FooterItem key={i} link={""} title={item} />
-              ))}
-              
-            </InfoSection>
+      <>
+          {/* Desktop Version */}
+          <div className="hidden px-6 lg:px-20 pt-16 flex-col border-[5px] bg-[#001c43] sm:flex gap-2">
+              <div className="flex justify-between items-start gap-6 lg:gap-10">
+                  {footerSectionsData.map((section, index) => (
+                      <InfoSection key={index} title={section.title}>
+                          {section.items.map((item, i) => (
+                              <FooterItem
+                                  key={i}
+                                  link={item.link}
+                                  title={item.title}
+                              />
+                          ))}
+                      </InfoSection>
+                  ))}
+                  <SubscribeForm />
+              </div>
+              <PaymentSocial />
 
-          ))}
-          <SubscribeForm />
-        </div>
-        <PaymentSocial />
+              <div className="w-full h-0 border border-neutral-400"></div>
 
-        <div className="w-full h-0 border border-neutral-400"></div>
-
-        <div className="flex flex-col items-center gap-2 mt-auto text-center">
-          <div className="text-[#94c4db] flex gap-5 ">
-            <span>Condition of use</span>
-            <span>Privacy Notice</span>
-            <span>Your Ads Privacy Choices</span>
+              <div className="flex flex-col items-center gap-2 mt-auto text-center">
+                  <div className="text-[#94c4db] flex gap-5 ">
+                      <span>Condition of use</span>
+                      <span>Privacy Notice</span>
+                      <span>Your Ads Privacy Choices</span>
+                  </div>
+                  <div className="text-[#94c4db]">
+                      &copy; 1998 - 2024, Yorkshoppers.com, Inc, or its
+                      affiliates
+                  </div>
+              </div>
           </div>
-          <div className="text-[#94c4db]">
-            &copy; 1998 - 2024, Yorkshoppers.com, Inc, or its affiliates
-          </div>
-        </div>
-      </div>
 
-      {/* Mobile Version */}
-      <div className="hidden px-6 py-8 justify-center items-center border-[5px] bg-[#001c43]   max-sm:flex flex-col gap-[72px]">
-        <div className="flex gap-[53px]">
-          {footerSectionsDataMobile.map((section, index) => (
-            <div key={index} className="flex flex-col gap-6">
-              {section.map((item, i) => (
-                <FooterItem key={i} link={""} title={item} />
-              ))}
-            </div>
-          ))}
-        </div>
+          {/* Mobile Version */}
+          <div className="hidden px-6 py-8 justify-center items-center border-[5px] bg-[#001c43]   max-sm:flex flex-col gap-[72px]">
+              <div className="flex gap-[53px]">
+                  {footerSectionsDataMobile.map((section, index) => (
+                      <div key={index} className="flex flex-col gap-6">
+                          {section.map((item, i) => (
+                              <FooterItem key={i} link={''} title={item} />
+                          ))}
+                      </div>
+                  ))}
+              </div>
 
-        <div className="flex flex-col items-center gap-2 mt-auto text-center text-xs font-[Roboto] font-normal">
-          <div className="text-[#2284B6] flex gap-5">
-            <span>Condition of use</span>
-            <span>Privacy Notice</span>
-            <span>Your Ads Privacy Choices</span>
+              <div className="flex flex-col items-center gap-2 mt-auto text-center text-xs font-[Roboto] font-normal">
+                  <div className="text-[#2284B6] flex gap-5">
+                      <span>Condition of use</span>
+                      <span>Privacy Notice</span>
+                      <span>Your Ads Privacy Choices</span>
+                  </div>
+                  <div className="text-[#2284B6]">
+                      &copy; 1998 - 2024, Yorkshoppers.com, Inc, or its
+                      affiliates
+                  </div>
+              </div>
           </div>
-          <div className="text-[#2284B6]">
-            &copy; 1998 - 2024, Yorkshoppers.com, Inc, or its affiliates
-          </div>
-        </div>
-      </div>
-    </>
+      </>
   );
 };
 

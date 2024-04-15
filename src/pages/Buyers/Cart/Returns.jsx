f0312c13e29e76863c96498ea9f14c2e9d9d2f3e
Akan <akabasu2010@gmail.com>
2024-04-15 07:36:20 -0700

    Creating PromoCode and Returns policy components.

---
+++ src/pages/Buyers/Cart/Returns.jsx
@@ -0,0 +1,16 @@
+function Returns() {
+
+    return (
+        <div className="border-8 border-gray-200 bg-white">
+            <div className="bg-white text-left m-3">
+                <h2 className="mb-8 text-xl font-bold">Returns are easy</h2>
+                <div className="flex flex-row gap-4">
+                    <p className="text-sm">Free return within 15 days for Official Store items and 7 days for other eligible items.
+                        <span className="text-sm text-blue-500 hover:text-blue-600 underline">See more</span> </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Returns;

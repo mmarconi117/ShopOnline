bbbedebeaf79d5a76918ea7d19cc64976928f903
zhi_jun <liangzhijun1216@gmail.com>
2024-04-16 23:10:30 -0400

    fixed banner image, the help center text container, and working on the search bar

--- src/components/Footer/Help-center/HelpCenter.jsx
+++ src/components/Footer/Help-center/HelpCenter.jsx
@@ -1,21 +1,17 @@
 import Banner from './Banner';
-import SearchBar from './SearchBar';
-import TopicsPrompt from './TopicsPrompt';
-import RecentPurchases from './RecentPurchases';
-import HelpTopics from './HelpTopics';
-import CommunicationOptions from './CommunicationOptions';
+import Search from './SearchBar';
+// import TopicsPrompt from './TopicsPrompt';
+// import RecentPurchases from './RecentPurchases';
+// import HelpTopics from './HelpTopics';
+// import CommunicationOptions from './CommunicationOptions';
 
 const HelpCenter = () => {
     return (
         <div id="buyer's-help-center-component">
             <Banner />
-            <div>
-                <SearchBar />
-                <TopicsPrompt />
-            </div>
-            <RecentPurchases />
-            <HelpTopics />
-            <CommunicationOptions />
+            <Search />
+
+
         </div>
     );
 };

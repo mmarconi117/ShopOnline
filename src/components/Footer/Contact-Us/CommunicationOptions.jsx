714596244408678e9380ed0e03e9381c3df657b9
GabrielOchieng <ogingagabriel@gmail.com>
2024-04-17 18:22:30 +0300

    Provided styling for contact in footer

--- src/components/Footer/Contact-Us/CommunicationOptions.jsx
+++ src/components/Footer/Contact-Us/CommunicationOptions.jsx
@@ -1,57 +1,76 @@
-import phoneImg from '../../../assets/IMAGES/Footer/Contact-Us/Phone/Rectangle-1.jpeg';
-import emailImg from '../../../assets/IMAGES/Footer/Contact-Us/Email/Rectangle-1.jpeg';
+import phoneImg from "../../../assets/IMAGES/Footer/Contact-Us/Phone/Rectangle-1.jpeg";
+import emailImg from "../../../assets/IMAGES/Footer/Contact-Us/Email/Rectangle-1.jpeg";
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import { toggleEmailModal } from '../../../reducersAndActions/actions/emailModalAction';
-import EmailModal from '../EmailModal/EmailModal';
+import { toggleEmailModal } from "../../../reducersAndActions/actions/emailModalAction";
+import EmailModal from "../EmailModal/EmailModal";
 const CommunicationOptions = () => {
-    const displayEmailModal = useSelector(
-        (state) => state.emailModalReducer.displayEmailModal,
-    );
+  const displayEmailModal = useSelector(
+    (state) => state.emailModalReducer.displayEmailModal
+  );
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const toggle = () => {
-        dispatch(toggleEmailModal(!displayEmailModal));
-    };
+  const toggle = () => {
+    dispatch(toggleEmailModal(!displayEmailModal));
+  };
 
-    return (
-        <div id="communication-options" className="my-5">
-            {displayEmailModal && <EmailModal />}
-            <div id="call-us-container" className="flex">
-                <div>
-                    <div>
-                        <p>Call US</p>
-                    </div>
+  return (
+    <div
+      id="communication-options"
+      className="my-5 p-10 flex flex-col gap-10 items-center justify-center"
+    >
+      {displayEmailModal && <EmailModal />}
+      <div
+        id="call-us-container"
+        className="flex max-[515px]:flex-col max-[515px]:h-auto h-32 md:h-48 w-full justify-end gap-10 md:gap-48 items-center border-0 bg-white"
+      >
+        <div className="flex flex-col items-center gap-2">
+          <div>
+            <p className=" text-blue-800 font-semibold max-[515px]:p-2">
+              Call US
+            </p>
+          </div>
 
-                    <div>
-                        <p>Give us a call: 1-800-123-4567</p>
-                        <p>Monday - Friday 6am - 5pm PST</p>
-                        <p>Saturday - Sunday 8am - 2pm PST</p>
-                    </div>
-                </div>
-                <div>
-                    <img src={phoneImg} alt="phone line image" />
-                </div>
-            </div>
-            <div id="email-us-container" className="flex">
-                <div>
-                    <div>
-                        <p>Email</p>
-                    </div>
-                    <div>
-                        <button onClick={() => toggle()}>
-                            nyshopping@shopping.com
-                        </button>
-                    </div>
-                </div>
-                <div>
-                    <img src={emailImg} alt="laptop typing image" />
-                </div>
-            </div>
+          <div className="max-[515px]:p-2">
+            <p className="mb-2">Give us a call: 1-800-123-4567</p>
+            <p>Monday - Friday 6am - 5pm PST</p>
+            <p>Saturday - Sunday 8am - 2pm PST</p>
+          </div>
         </div>
-    );
+        <div className="max-[515px]:w-full">
+          <img
+            src={phoneImg}
+            alt="phone line image"
+            className="md:h-48 h-32 w-32 md:w-48 object-cover max-[515px]:w-full"
+          />
+        </div>
+      </div>
+      <div
+        id="email-us-container"
+        className="flex max-[515px]:flex-col max-[515px]:h-auto h-32 md:h-48 w-full justify-end gap-10 md:gap-48 items-center border-0 bg-white"
+      >
+        <div className="flex flex-col items-center gap-2">
+          <div>
+            <p className="text-blue-800 font-semibold max-[515px]:pt-2">
+              Email
+            </p>
+          </div>
+          <div>
+            <button onClick={() => toggle()}>nyshopping@shopping.com</button>
+          </div>
+        </div>
+        <div className="max-[515px]:w-full">
+          <img
+            src={emailImg}
+            alt="laptop typing image"
+            className="w-32 md:w-48 h-32 md:h-48 object-cover max-[515px]:w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CommunicationOptions;
--- src/components/Footer/Contact-Us/QuickLinks.jsx
+++ src/components/Footer/Contact-Us/QuickLinks.jsx
@@ -1,50 +1,65 @@
-import rightArrow from '../../../assets/ICONS/RightArrow.svg';
+import rightArrow from "../../../assets/ICONS/RightArrow.svg";
 
 const QuickLinks = () => {
-    const items = [
-        {
-            name: 'Track Order',
-            icon: rightArrow,
-            link: '/orders',
-        },
-        {
-            name: 'Help Page',
-            icon: rightArrow,
-            link: '/help-center',
-        },
-        {
-            name: "FAQ's",
-            icon: rightArrow,
-            link: '/faqs',
-        },
-    ];
+  const items = [
+    {
+      name: "Track Order",
+      icon: rightArrow,
+      link: "/orders",
+    },
+    {
+      name: "Help Page",
+      icon: rightArrow,
+      link: "/help-center",
+    },
+    {
+      name: "FAQ's",
+      icon: rightArrow,
+      link: "/faqs",
+    },
+  ];
 
-    const options = items.map((item, index) => {
-        return (
-            <div className="flex" key={index}>
-                <div>
-                    <a href={item.link}>{item.name}</a>
-                </div>
-                <div>
-                    <img src={item.icon} />
-                </div>
-            </div>
-        );
-    });
-
+  const options = items.map((item, index) => {
     return (
-        <div id="quick-links-component" className="my-5">
-            <div>
-                <div>
-                    <p> Quick Links!</p>
-                </div>
-                <div>
-                    <p>Please check out our help page & FAQ's</p>
-                </div>
-            </div>
-            <div className="flex">{options}</div>
+      <div
+        className="flex border-0 w-28 md:w-48 lg:w-64 h-32 justify-center items-center gap-2 bg-gray-100 shadow-md max-[515px]:w-[90%]"
+        key={index}
+      >
+        <div>
+          <a
+            href={item.link}
+            className="font-bold text-blue-900 hover:text-blue-500"
+          >
+            {item.name}
+          </a>
         </div>
+        <div>
+          <img src={item.icon} />
+        </div>
+      </div>
     );
+  });
+
+  return (
+    <div className="bg-white">
+      <div id="quick-links-component" className="my-5 p-10">
+        <div className="mb-4">
+          <div>
+            <p className="text-blue-900 font-bold"> Quick Links!</p>
+          </div>
+          <div>
+            <p className="text-blue-900 font-medium">
+              {" "}
+              Please check out our help page & FAQ's
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-between max-[500px]:flex-col max-[515px]:gap-4 max-[515px]:items-center">
+          {options}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuickLinks;

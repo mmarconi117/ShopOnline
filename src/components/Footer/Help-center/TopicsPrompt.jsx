6d49126db9600590247801e02eb2b96a506106bf
zhi_jun <liangzhijun1216@gmail.com>
2024-04-18 19:15:44 -0400

    fixed topic prompts and responsive to mobile too

--- src/components/Footer/Help-center/TopicsPrompt.jsx
+++ src/components/Footer/Help-center/TopicsPrompt.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setTopicPrompt } from '../../../reducersAndActions/actions/helpCenterAction';
 
@@ -11,21 +12,40 @@ const TopicsPrompt = () => {
         dispatch(setTopicPrompt(topicIndex));
     };
 
-    const navItems = topicsPrompt.map((item, index) => {
-        return (
-            <li key={index}>
-                <button onClick={() => selectTopicPrompt(index)}>
-                    {item.name}
-                </button>
-            </li>
-        );
-    });
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const isMobile = windowWidth <= 640;
+
+    const numItemsPerRow = isMobile ? 3 : 7;
+
+    const rows = [];
+    for (let i = 0; i < topicsPrompt.length; i += numItemsPerRow) {
+        rows.push(topicsPrompt.slice(i, i + numItemsPerRow));
+    }
+
+    const navItems = rows.map((row, index) => (
+        <div key={index} className={`flex flex-row flex-wrap justify-center ${isMobile ? 'mb-4' : 'mb-8'}`}>
+            {row.map((item, itemIndex) => (
+                <div key={itemIndex} className={`border p-2 text-center rounded-2xl ${isMobile ? 'w-32 mx-1 my-1' : 'md:mx-2'}`} style={{ borderColor: 'black' }}>
+                    <button onClick={() => selectTopicPrompt(index * numItemsPerRow + itemIndex)}>
+                        {item.name}
+                    </button>
+                </div>
+            ))}
+        </div>
+    ));
 
     return (
-        <div id="help-center-nav" className="my-5">
-            <nav>
-                <ul>{navItems}</ul>
-            </nav>
+        <div id="help-center-nav" className="my-5 flex flex-col items-center">
+            {navItems}
         </div>
     );
 };

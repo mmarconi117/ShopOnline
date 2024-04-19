8373c0f268a445b3c57b7a61cb393da8f93f8eef
zhi_jun <liangzhijun1216@gmail.com>
2024-04-19 15:20:25 -0400

    responsive for communication option

--- src/components/Footer/Help-center/CommunicationOptions.jsx
+++ src/components/Footer/Help-center/CommunicationOptions.jsx
@@ -1,68 +1,72 @@
-import callUsImg from '../../../assets/IMAGES/Footer/Call-us/Rectangle-1.jpeg';
-import emailUs from '../../../assets/IMAGES/Footer/email-us/Rectangle-1.jpeg';
-import phoneIcon from '../../../assets/ICONS/Phone.svg';
-import emailIcon from '../../../assets/ICONS/Email.svg';
-import EmailModal from '../EmailModal/EmailModal';
+import callUsImg from "../../../assets/IMAGES/Footer/Call-us/Rectangle-1.jpeg";
+import emailUs from "../../../assets/IMAGES/Footer/email-us/Rectangle-1.jpeg";
+import phoneIcon from "../../../assets/ICONS/Phone.svg";
+import messageImage from "../../../assets/ICONS/MessageUs.svg";
+import emailIcon from "../../../assets/ICONS/Email.svg";
+import messageIcon from "../../../assets/ICONS/MessageIcon.svg";
+import EmailModal from "../EmailModal/EmailModal";
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import { toggleEmailModal } from '../../../reducersAndActions/actions/emailModalAction';
+import { toggleEmailModal } from "../../../reducersAndActions/actions/emailModalAction";
 
 const CommunicationOptions = () => {
-    const displayEmailModal = useSelector(
-        (state) => state.emailModalReducer.displayEmailModal,
-    );
+  const displayEmailModal = useSelector(
+    (state) => state.emailModalReducer.displayEmailModal
+  );
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const items = [
-        {
-            description: 'Call Us',
-            icon: phoneIcon,
-            img: callUsImg,
-        },
-        {
-            description: 'Email',
-            icon: emailIcon,
-            img: emailUs,
-        },
-    ];
+  const items = [
+    {
+      description: "Call Us",
+      icon: phoneIcon,
+      img: callUsImg,
+    },
+    {
+      description: "Email",
+      icon: emailIcon,
+      img: emailUs,
+    },
+    {
+      description: "Message us",
+      icon: messageIcon,
+      img: messageImage,
+    },
+  ];
 
-    const toggle = () => {
-        dispatch(toggleEmailModal(!displayEmailModal));
-    };
+  const toggle = () => {
+    dispatch(toggleEmailModal(!displayEmailModal));
+  };
 
-    const commOptions = items.map((item, index) => {
-        return (
-            <div key={index}>
-                <div>
-                    <img
-                        src={item.img}
-                        className="w-80 h-60"
-                        alt={item.description}
-                    />
-                </div>
-                <div className="flex my-5">
-                    <img
-                        className="w-6 h-6"
-                        src={item.icon}
-                        alt={item.description}
-                    />
-                    <button onClick={() => toggle()}>{item.description}</button>
-                </div>
-            </div>
-        );
-    });
-
+  const commOptions = items.map((item, index) => {
     return (
-        <div id="help-center-comunnication-options">
-            <div>
-                <p>Need Help? We are here for you.</p>
-            </div>
-            {displayEmailModal && <EmailModal />}
-            <div>{commOptions}</div>
+      <div
+        key={index}
+        className="option-container flex flex-col items-center space-y-4 p-10 md:p-0"
+      >
+        <div className="bg-[#e9f0f5] p-16  rounded-lg hidden md:block md:m-10">
+          <img src={item.img} className="w-80 h-60" alt={item.description} />
         </div>
+        <div className="flex items-center justify-center bg-white px-10 py-5 rounded-lg shadow-lg md:flex-row md:items-start md:justify-center md:py-3  ">
+          <img
+            className="w-6 h-6 mr-3  md:mb-3"
+            src={item.icon}
+            alt={item.description}
+          />
+          <button onClick={() => toggle()}>{item.description}</button>
+        </div>
+      </div>
     );
+  });
+
+  return (
+    <div id="help-center-comunnication-options" className="px-10 w-full">
+      <p className="font-bold text-blue-700 text-[24px]">Need Help? We are here for you.</p>
+      {displayEmailModal && <EmailModal />}
+      <div className="flex justify-center items-center  md:mb-6 ">{commOptions}</div>
+    </div>
+  );
 };
 
 export default CommunicationOptions;

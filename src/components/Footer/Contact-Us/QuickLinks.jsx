f816523ed60708643552182efc80d91d1ab768b6
GabrielOchieng <ogingagabriel@gmail.com>
2024-04-20 13:38:47 +0300

    Corrected mobile responsiveness based on figma

--- src/components/Footer/Contact-Us/QuickLinks.jsx
+++ src/components/Footer/Contact-Us/QuickLinks.jsx
@@ -1,4 +1,5 @@
 import rightArrow from "../../../assets/ICONS/RightArrow.svg";
+import LocationIcon from "../../../assets/ICONS/location-marker.svg";
 
 const QuickLinks = () => {
   const items = [
@@ -22,7 +23,7 @@ const QuickLinks = () => {
   const options = items.map((item, index) => {
     return (
       <div
-        className="flex border-0 w-28 md:w-48 lg:w-64 h-32 justify-center items-center gap-2 bg-gray-100 shadow-md max-[515px]:w-[90%]"
+        className="flex border-0 max-[500px]:h-28 w-28 md:w-48 lg:w-64 h-32 justify-center items-center gap-2 bg-gray-100 shadow-md"
         key={index}
       >
         <div>
@@ -34,7 +35,7 @@ const QuickLinks = () => {
           </a>
         </div>
         <div>
-          <img src={item.icon} />
+          <img src={item.icon} className="hidden sm:block " />
         </div>
       </div>
     );
@@ -42,20 +43,40 @@ const QuickLinks = () => {
 
   return (
     <div className="bg-white">
-      <div id="quick-links-component" className="my-5 p-10">
-        <div className="mb-4">
+      <div
+        id="quick-links-component"
+        className="p-5 sm:p-10 flex flex-col gap-10"
+      >
+        <div>
           <div>
             <p className="text-blue-900 font-bold"> Quick Links!</p>
           </div>
           <div>
-            <p className="text-blue-900 font-medium">
+            <p className="text-blue-900 font-medium hidden sm:block">
               {" "}
               Please check out our help page & FAQ's
             </p>
           </div>
         </div>
-        <div className="flex justify-between max-[500px]:flex-col max-[515px]:gap-4 max-[515px]:items-center">
+        <div className="flex justify-between max-[500px]:text-xs max-[515px]:gap-4">
           {options}
+        </div>
+        {/* Location information for mobile design  */}
+
+        <div className=" sm:hidden flex py-5 px-2  items-center gap-5  bg-gray-100 shadow-md ">
+          <div className="ml-3">
+            <img
+              src={LocationIcon}
+              alt="email icon"
+              className="w-12 h-12 bg-gray-300 border rounded-full p-2"
+            />
+          </div>
+          <div>
+            <h1 className="font-bold text-sm">Newyork</h1>
+            <p className="text-gray-500 text-sm">
+              100 Flinders Street, New York VIC 3000 US
+            </p>
+          </div>
         </div>
       </div>
     </div>

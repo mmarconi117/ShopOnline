b5e024dc7efcb24edb842ff394f2bf679ef6d43f
zhi_jun <liangzhijun1216@gmail.com>
2024-04-21 16:19:42 -0400

    added responsive for banner in about us

--- src/components/Footer/About-Us/Banner.jsx
+++ src/components/Footer/About-Us/Banner.jsx
@@ -1,22 +1,15 @@
-import banner from '../../../assets/IMAGES/Footer/Contact-Us/Rectangle-1.jpeg';
+import banner from '../../../assets/IMAGES/Footer/Contact-Us/Rectangle-2.svg';
 
 const Banner = () => {
-    return (
-        <div id="banner component">
-            <div className="bg-[url(../../src/assets/IMAGES/Footer/Contact-Us/Rectangle-1.jpeg)] w-full h-[700px]">
-                {/* <img src={banner} alt="galaxy picture background" /> */}
-                <div className="p-2 bg-slate-400 w-1/6 m-auto">
-                    <div className="p-2 bg-slate-300">
-                        <div className="p-2 bg-slate-200">
-                            <p className="text-black p-2 bg-white text-center">
-                                About Us
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="banner component">
+      <div className="bg-[url(../../src/assets/IMAGES/Footer/Contact-Us/Rectangle-1.jpeg)] w-full h-[325px]">
+        <div className="flex items-center flex-row justify-center p-24 w-full">
+          <img src={banner} className=''></img>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;

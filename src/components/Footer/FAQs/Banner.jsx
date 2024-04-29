88548e955fb3a47bba0ccc4c57581d09cf0fa9bf
brandonom <brandonomyers1@gmail.com>
2024-04-29 16:24:06 -0400

    updated styling for banner

--- src/components/Footer/FAQs/Banner.jsx
+++ src/components/Footer/FAQs/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
                 <div className="p-2 bg-slate-400 w-1/3 h-34 m-auto">
                     <div className="p-2 bg-slate-300 h-30">
                         <div className="p-2 bg-slate-200 h-28">
-                            <p className="text-black p-4 bg-white text-center lg:text-6xl md:text-3xl sm:text-lg font-bold h-24 text-md">
+                            <p className="text-black p-4 bg-white text-center lg:text-6xl md:text-3xl sm:text-lg font-bold h-22 text-md">
                                 FAQ'S
                             </p>
                         </div>
